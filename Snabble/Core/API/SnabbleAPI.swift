9cfc873d7af10b84217c0622501886fd83699586
Gereon Steffens <gereon@snabble.io>
2018-12-07 11:50:01 +0100

    add backup CAs

--- Snabble/Core/API/SnabbleAPI.swift
+++ Snabble/Core/API/SnabbleAPI.swift
@@ -161,6 +161,17 @@ extension SnabbleAPI {
 /// Trustkit / certificate pinning
 extension SnabbleAPI {
 
+    private static let CAHashes = [
+        // let's encrypt
+        "YLh1dUR9y6Kja30RrAn7JKnbQG/uEtLMkBgFF2Fuihg=", "sRHdihwgkaib1P1gxX8HFszlD+7/gTfNvuAybgLPNis=",
+        // backup CAs
+        "C5+lpZ7tcVwmwQIMcRtPbsQtWLABXhQzejna0wHFr8M=", "lCppFqbkrlJ3EcVFAkeip0+44VaoJUymbnOaEUk7tEU=",
+        "r/mIkG3eEpVdm+u/ko/cwxzOMo1bk4TyHIlByibiA5E=", "i7WTqTvh0OioIruIfFR4kMPnBqrS2rdiVPl/s2uC/CY=",
+        "WoiWRyIOVNa9ihaBciRSC7XHjliYS9VwUGOIud4PB18=", "h6801m+z8v3zbgkRHpq6L29Esgfzhj89C1SyUCOQmqU=",
+        "q5hJUnat8eyv8o81xTBIeB5cFxjaucjmelBPT2pRMo8=", "47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=",
+        "SQVGZiOrQXi+kqxcvWWE96HhfydlLVqFr4lQTqI5qqo="
+    ]
+
     private static func initializeTrustKit() {
         let trustKitConfig: [String: Any] = [
             kTSKSwizzleNetworkDelegates: false,
@@ -169,25 +180,19 @@ extension SnabbleAPI {
                     kTSKExpirationDate: "2021-03-17",
                     kTSKIncludeSubdomains: true,
                     kTSKDisableDefaultReportUri: true,
-                    kTSKPublicKeyHashes: [
-                        "YLh1dUR9y6Kja30RrAn7JKnbQG/uEtLMkBgFF2Fuihg=", "sRHdihwgkaib1P1gxX8HFszlD+7/gTfNvuAybgLPNis="  // let's encrypt
-                    ],
+                    kTSKPublicKeyHashes: SnabbleAPI.CAHashes
                 ],
                 "snabble-testing.io": [
                     kTSKExpirationDate: "2021-03-17",
                     kTSKIncludeSubdomains: true,
                     kTSKDisableDefaultReportUri: true,
-                    kTSKPublicKeyHashes: [
-                        "YLh1dUR9y6Kja30RrAn7JKnbQG/uEtLMkBgFF2Fuihg=", "sRHdihwgkaib1P1gxX8HFszlD+7/gTfNvuAybgLPNis="  // let's encrypt
-                    ],
+                    kTSKPublicKeyHashes: SnabbleAPI.CAHashes
                 ],
                 "snabble-staging.io": [
                     kTSKExpirationDate: "2021-03-17",
                     kTSKIncludeSubdomains: true,
                     kTSKDisableDefaultReportUri: true,
-                    kTSKPublicKeyHashes: [
-                        "YLh1dUR9y6Kja30RrAn7JKnbQG/uEtLMkBgFF2Fuihg=", "sRHdihwgkaib1P1gxX8HFszlD+7/gTfNvuAybgLPNis="  // let's encrypt
-                    ],
+                    kTSKPublicKeyHashes: SnabbleAPI.CAHashes
                 ]
             ]
         ]
