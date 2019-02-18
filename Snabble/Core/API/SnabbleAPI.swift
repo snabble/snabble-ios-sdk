2a57be4a244dcd00bf56460f910020986713c9bb
Gereon Steffens <gereon@snabble.io>
2019-02-18 10:55:52 +0100

    move `Log` enum to Utilities.swift

--- Snabble/Core/API/SnabbleAPI.swift
+++ Snabble/Core/API/SnabbleAPI.swift
@@ -191,21 +191,3 @@ func synchronized<T>(_ lock: Any, closure: () throws -> T) rethrows -> T {
     return try closure()
 }
 
-/// Logging
-enum Log {
-    static func info(_ str: String) {
-        NSLog("SnabbleSDK INFO: %@", str)
-    }
-
-    static func debug(_ str: String) {
-        NSLog("SnabbleSDK DEBUG: %@", str)
-    }
-
-    static func warn(_ str: String) {
-        NSLog("SnabbleSDK WARN: %@", str)
-    }
-
-    static func error(_ str: String) {
-        NSLog("SnabbleSDK ERROR: %@", str)
-    }
-}
--- Snabble/Core/API/Utilities.swift
+++ Snabble/Core/API/Utilities.swift
@@ -4,6 +4,8 @@
 //  Copyright © 2019 snabble. All rights reserved.
 //
 
+// MARK: - string-based enums
+
 /// for RawRepresentable enums, define a `unknownCase` fallback and a non-failable initializer
 protocol UnknownCaseRepresentable: RawRepresentable, CaseIterable where RawValue: Equatable {
     static var unknownCase: Self { get }
@@ -13,5 +15,24 @@ extension UnknownCaseRepresentable {
     public init(rawValue: RawValue) {
         let value = Self.allCases.first(where: { $0.rawValue == rawValue })
         self = value ?? Self.unknownCase
+    }
+}
+
+// MARK: - Logging
+enum Log {
+    static func info(_ str: String) {
+        NSLog("SnabbleSDK INFO: %@", str)
+    }
+
+    static func debug(_ str: String) {
+        NSLog("SnabbleSDK DEBUG: %@", str)
+    }
+
+    static func warn(_ str: String) {
+        NSLog("SnabbleSDK WARN: %@", str)
+    }
+
+    static func error(_ str: String) {
+        NSLog("SnabbleSDK ERROR: %@", str)
     }
 }
