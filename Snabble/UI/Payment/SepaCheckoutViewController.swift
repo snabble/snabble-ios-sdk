fcc342c88dab85035a0f256a807e4a18f48d6514
Gereon Steffens <gereon@snabble.io>
2019-05-17 09:50:17 +0200

    disable back button in SEPA checkout view

--- Snabble/UI/Payment/SepaCheckoutViewController.swift
+++ Snabble/UI/Payment/SepaCheckoutViewController.swift
@@ -51,6 +51,8 @@ final class SepaCheckoutViewController: UIViewController {
         self.codeWidth.constant = self.codeImage.image?.size.width ?? 0
 
         self.cancelButton.setTitle("Snabble.Cancel".localized(), for: .normal)
+
+        self.navigationItem.hidesBackButton = true
     }
 
     override func viewWillAppear(_ animated: Bool) {
