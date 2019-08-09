7cd493bcc8a31cabbee841c2f6385c8696a44f13
Gereon Steffens <gereon@snabble.io>
2019-08-09 16:28:48 +0200

    split ShoppingCart into multiple files

---
+++ Snabble/Core/Cart/CartEvent.swift
@@ -0,0 +1,46 @@
+//
+//  CartEvent
+//
+//  Copyright © 2019 snabble. All rights reserved.
+//
+
+import Foundation
+
+// MARK: send events
+struct CartEvent {
+    static func sessionStart(_ cart: ShoppingCart) {
+        guard cart.shopId != "" else {
+            return
+        }
+
+        guard let project = SnabbleAPI.projectFor(cart.projectId) else {
+            return
+        }
+
+        let event = AppEvent(.sessionStart, session: cart.session, project: project, shopId: cart.shopId)
+        event.post()
+    }
+
+    static func sessionEnd(_ cart: ShoppingCart) {
+        guard cart.shopId != "" else {
+            return
+        }
+
+        guard let project = SnabbleAPI.projectFor(cart.projectId) else {
+            return
+        }
+
+        let event = AppEvent(.sessionEnd, session: cart.session, project: project, shopId: cart.shopId)
+        event.post()
+    }
+
+    static func cart(_ cart: ShoppingCart) {
+        if cart.shopId == "" || (cart.items.count == 0 && cart.session == "") {
+            return
+        }
+
+        cart.createCheckoutInfo(completion: {_ in})
+        let event = AppEvent(cart)
+        event?.post()
+    }
+}
