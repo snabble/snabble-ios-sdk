1855d17368d835368763d626f15d844ca6d92024
Gereon Steffens <gereon@snabble.io>
2020-02-26 12:19:42 +0100

    add publish workflow

--- pod-release.sh
+++ pod-release.sh
@@ -44,5 +44,5 @@ git add .
 git commit -m "release v$POD_VERSION"
 git tag $POD_VERSION
 git push origin master --tags
-unset SNABBLE_DEV
-pod trunk push Snabble.podspec --allow-warnings
+# unset SNABBLE_DEV
+# pod trunk push Snabble.podspec --allow-warnings
