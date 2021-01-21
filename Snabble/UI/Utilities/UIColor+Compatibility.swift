8521ccfeedef7793de8452265278fdd4643f2a8d
Andreas Osberghaus <andreas.osberghaus@snabble.io>
2021-01-21 14:25:26 +0100

    colors are only internal

--- Snabble/UI/Utilities/UIColor+Compatibility.swift
+++ Snabble/UI/Utilities/UIColor+Compatibility.swift
@@ -10,99 +10,99 @@ import UIKit
 import ColorCompatibility
 
 extension UIColor {
-    public static var label: UIColor {
+    static var label: UIColor {
         ColorCompatibility.label
     }
 
-    public static var secondaryLabel: UIColor {
+    static var secondaryLabel: UIColor {
         ColorCompatibility.secondaryLabel
     }
 
-    public static var tertiaryLabel: UIColor {
+    static var tertiaryLabel: UIColor {
         ColorCompatibility.tertiaryLabel
     }
 
-    public static var quaternaryLabel: UIColor {
+    static var quaternaryLabel: UIColor {
         ColorCompatibility.quaternaryLabel
     }
 
-    public static var systemFill: UIColor {
+    static var systemFill: UIColor {
         ColorCompatibility.systemFill
     }
 
-    public static var secondarySystemFill: UIColor {
+    static var secondarySystemFill: UIColor {
         ColorCompatibility.secondarySystemFill
     }
 
-    public static var tertiarySystemFill: UIColor {
+    static var tertiarySystemFill: UIColor {
         ColorCompatibility.tertiarySystemFill
     }
 
-    public static var quaternarySystemFill: UIColor {
+    static var quaternarySystemFill: UIColor {
         ColorCompatibility.quaternarySystemFill
     }
 
-    public static var placeholderText: UIColor {
+    static var placeholderText: UIColor {
         ColorCompatibility.placeholderText
     }
 
-    public static var systemBackground: UIColor {
+    static var systemBackground: UIColor {
         ColorCompatibility.systemBackground
     }
 
-    public static var secondarySystemBackground: UIColor {
+    static var secondarySystemBackground: UIColor {
         ColorCompatibility.secondarySystemBackground
     }
 
-    public static var tertiarySystemBackground: UIColor {
+    static var tertiarySystemBackground: UIColor {
         ColorCompatibility.tertiarySystemBackground
     }
 
-    public static var systemGroupedBackground: UIColor {
+    static var systemGroupedBackground: UIColor {
         ColorCompatibility.systemGroupedBackground
     }
 
-    public static var secondarySystemGroupedBackground: UIColor {
+    static var secondarySystemGroupedBackground: UIColor {
         ColorCompatibility.secondarySystemGroupedBackground
     }
 
-    public static var tertiarySystemGroupedBackground: UIColor {
+    static var tertiarySystemGroupedBackground: UIColor {
         ColorCompatibility.tertiarySystemGroupedBackground
     }
 
-    public static var separator: UIColor {
+    static var separator: UIColor {
         ColorCompatibility.separator
     }
 
-    public static var opaqueSeparator: UIColor {
+    static var opaqueSeparator: UIColor {
         ColorCompatibility.opaqueSeparator
     }
 
-    public static var link: UIColor {
+    static var link: UIColor {
         ColorCompatibility.link
     }
 
-    public static var systemIndigo: UIColor {
+    static var systemIndigo: UIColor {
         ColorCompatibility.systemIndigo
     }
 
-    public static var systemGray2: UIColor {
+    static var systemGray2: UIColor {
         ColorCompatibility.systemGray2
     }
 
-    public static var systemGray3: UIColor {
+    static var systemGray3: UIColor {
         ColorCompatibility.systemGray3
     }
 
-    public static var systemGray4: UIColor {
+    static var systemGray4: UIColor {
         ColorCompatibility.systemGray4
     }
 
-    public static var systemGray5: UIColor {
+    static var systemGray5: UIColor {
         ColorCompatibility.systemGray5
     }
 
-    public static var systemGray6: UIColor {
+    static var systemGray6: UIColor {
         ColorCompatibility.systemGray6
     }
 }
