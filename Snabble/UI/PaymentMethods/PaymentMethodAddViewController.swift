1119aaa1f90727a4114b5062cd20d71a4c28b245
Gereon Steffens <gereon@snabble.io>
2021-02-15 15:44:26 +0100

    extract cell classes into their own files

--- Snabble/UI/PaymentMethods/PaymentMethodAddViewController.swift
+++ Snabble/UI/PaymentMethods/PaymentMethodAddViewController.swift
@@ -8,30 +8,6 @@ import UIKit
 import ColorCompatibility
 import SDCAlertView
 
-private struct MethodEntry {
-    var name: String
-    let method: RawPaymentMethod?
-    let brandId: Identifier<Brand>?
-    let projectId: Identifier<Project>?
-    var count: Int
-
-    init(project: Project, count: Int) {
-        self.name = project.name
-        self.method = nil
-        self.brandId = project.brandId
-        self.projectId = project.id
-        self.count = count
-    }
-
-    init(method: RawPaymentMethod, count: Int, for project: Project? = nil) {
-        self.name = method.displayName
-        self.method = method
-        self.brandId = nil
-        self.projectId = project?.id
-        self.count = count
-    }
-}
-
 public final class PaymentMethodAddViewController: UITableViewController {
 
     private var entries = [[MethodEntry]]()
@@ -88,6 +64,7 @@ public final class PaymentMethodAddViewController: UITableViewController {
 
         // special case: remove externalBilling entries where we have no data
         for idx in 0..<entries.count {
+            // swiftlint:disable:next empty_count
             entries[idx].removeAll { $0.method == .externalBilling && $0.count == 0 }
         }
 
@@ -332,80 +309,4 @@ extension PaymentMethodAddViewController: ReactNativeWrapper {
             self.viewWillAppear(true)
         }
     }
-}
-
-private final class PaymentMethodAddCell: UITableViewCell {
-    private var icon: UIImageView
-    private var nameLabel: UILabel
-    private var countLabel: UILabel
-
-    var entry: MethodEntry? {
-        didSet {
-            icon.image = entry?.method?.icon
-            nameLabel.text = entry?.name
-
-            if let projectId = entry?.projectId, entry?.method == nil {
-                SnabbleUI.getAsset(.storeIcon, projectId: projectId) { img in
-                    self.icon.image = img
-                }
-            }
-            if let count = entry?.count {
-                countLabel.text = "\(count)"
-            } else {
-                countLabel.text = nil
-            }
-        }
-    }
-
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        icon = UIImageView()
-        nameLabel = UILabel()
-        countLabel = UILabel()
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-
-        icon.translatesAutoresizingMaskIntoConstraints = false
-        nameLabel.translatesAutoresizingMaskIntoConstraints = false
-        countLabel.translatesAutoresizingMaskIntoConstraints = false
-
-        contentView.addSubview(icon)
-        contentView.addSubview(nameLabel)
-        contentView.addSubview(countLabel)
-
-        countLabel.font = UIFont.systemFont(ofSize: 15, weight: .regular)
-        countLabel.textColor = ColorCompatibility.systemGray2
-
-        self.accessoryType = .disclosureIndicator
-
-        let noImageWidthConstraint = icon.widthAnchor.constraint(equalToConstant: 0)
-        noImageWidthConstraint.priority = .defaultHigh
-
-        icon.setContentHuggingPriority(.defaultHigh, for: .horizontal)
-        nameLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
-        countLabel.setContentHuggingPriority(.defaultHigh, for: .horizontal)
-        NSLayoutConstraint.activate([
-            icon.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
-            icon.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            icon.heightAnchor.constraint(equalToConstant: 24),
-            noImageWidthConstraint,
-
-            nameLabel.leadingAnchor.constraint(equalTo: icon.trailingAnchor, constant: 16),
-            nameLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-
-            countLabel.leadingAnchor.constraint(equalTo: nameLabel.trailingAnchor, constant: 16),
-            countLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
-            countLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
-        ])
-    }
-
-    override func prepareForReuse() {
-        super.prepareForReuse()
-
-        icon.image = nil
-        nameLabel.text = nil
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
 }
--- Snabble/UI/PaymentMethods/PaymentMethodListViewController.swift
+++ Snabble/UI/PaymentMethods/PaymentMethodListViewController.swift
@@ -212,56 +212,6 @@ extension PaymentMethodListViewController {
     }
 }
 
-private final class PaymentMethodListCell: UITableViewCell {
-    var method: PaymentMethodDetail? {
-        didSet {
-            self.nameLabel.text = method?.displayName
-            self.icon.image = method?.icon
-
-            if method?.originType == .tegutEmployeeID {
-                self.accessoryType = .none
-            } else {
-                self.accessoryType = .disclosureIndicator
-            }
-        }
-    }
-
-    private var nameLabel = UILabel()
-    private var icon = UIImageView()
-
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-
-        accessoryType = .disclosureIndicator
-
-        nameLabel.translatesAutoresizingMaskIntoConstraints = false
-        icon.translatesAutoresizingMaskIntoConstraints = false
-        contentView.addSubview(nameLabel)
-        contentView.addSubview(icon)
-
-        NSLayoutConstraint.activate([
-            icon.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
-            icon.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            icon.widthAnchor.constraint(equalToConstant: 38),
-
-            nameLabel.leadingAnchor.constraint(equalTo: icon.trailingAnchor, constant: 16),
-            nameLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
-            nameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16)
-        ])
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    override func prepareForReuse() {
-        super.prepareForReuse()
-
-        nameLabel.text = nil
-        icon.image = nil
-    }
-}
-
 // stuff that's only used by the RN wrapper
 extension PaymentMethodListViewController: ReactNativeWrapper {
     public func setMethod(_ method: RawPaymentMethod) {
