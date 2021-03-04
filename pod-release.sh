8711fdec442e255835521dccaf48f03c60d1d966
Gereon Steffens <gereon@snabble.io>
2021-03-04 11:31:47 +0100

    check building the RN wrapper when releasing

--- pod-release.sh
+++ pod-release.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -o pipefail
+
 TODAY=$(date +%Y-%m-%d)
 CHANGELOG_DATE=$(stat -f "%Sm" -t "%Y-%m-%d" CHANGELOG.md)
 
@@ -21,19 +23,35 @@ if [ "$TODAY" != "$CHANGELOG_DATE" ]; then
 fi
 
 echo running unit tests...
-if (cd ../iOS-App; bundle exec fastlane unittests); then
-    echo "passed!"
+if (cd ../iOS-App; bundle install >/dev/null; bundle exec fastlane unittests); then
+    echo "unit tests passed!"
 else
     echo "tests failed"
     exit 1
 fi
 
 echo building sample app...
-if (cd Example; pod install; xcodebuild -scheme Snabble-Example -workspace Snabble.xcworkspace build); then
-    echo "passed!"
+if (cd Example; pod install; xcodebuild -scheme Snabble-Example -workspace Snabble.xcworkspace build | xcpretty); then
+    echo "sample app passed!"
 else
     echo "build failed"
     exit 1
+fi
+
+echo checking RN wrapper compatibility...
+if [ -d ../react-native-snabble ]; then
+    (
+    cd ../react-native-snabble
+    perl -pi -e "s/, \"= .*\"/, \"= $POD_VERSION\"/" ios/Snabble-ReactNative.podspec
+    cd Sample/ios
+    pod install
+    if SKIP_BUNDLING=YES xcodebuild CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO CODE_SIGN_ENTITLEMENTS="" CODE_SIGN_IDENTITY="" -scheme Sample -workspace Sample.xcworkspace -configuration Debug build | xcpretty; then
+        echo "building RN sample passed"
+    else
+        echo "building RN sample failed"
+        exit 1
+    fi
+    )
 fi
 
 echo "updating strings file..."
@@ -43,5 +61,3 @@ git add .
 git commit -m "release v$POD_VERSION"
 git tag $POD_VERSION
 git push origin main --tags
-# unset SNABBLE_DEV
-# pod trunk push Snabble.podspec --allow-warnings
