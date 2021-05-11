f83992fa4a2483a08ee7ae9196f64f11a3146b18
Gereon Steffens <gereon@snabble.io>
2021-05-11 16:13:08 +0200

    fix formatting + name

--- Snabble/UI/Utilities/UIColor+HEX.swift
+++ Snabble/UI/Utilities/UIColor+HEX.swift
@@ -8,22 +8,22 @@ import Foundation
 import UIKit
 
 extension UIColor {
-  convenience init(rgbaValue: UInt32) {
-    let obaque = UInt32(0xff)
-    let red = CGFloat((rgbaValue >> 24) & obaque) / 255.0
-    let green = CGFloat((rgbaValue >> 16) & obaque) / 255.0
-    let blue = CGFloat((rgbaValue >> 8) & obaque) / 255.0
-    let alpha = CGFloat((rgbaValue) & obaque) / 255.0
+    convenience init(rgbaValue: UInt32) {
+        let byte: UInt32 = 0xff
+        let red = CGFloat((rgbaValue >> 24) & byte) / 255.0
+        let green = CGFloat((rgbaValue >> 16) & byte) / 255.0
+        let blue = CGFloat((rgbaValue >> 8) & byte) / 255.0
+        let alpha = CGFloat((rgbaValue) & byte) / 255.0
 
-    self.init(red: red, green: green, blue: blue, alpha: alpha)
-  }
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
 
-  convenience init(rgbValue: UInt32) {
-    let obaque = UInt32(0xff)
-    let red = CGFloat((rgbValue >> 16) & obaque) / 255.0
-    let green = CGFloat((rgbValue >> 8) & obaque) / 255.0
-    let blue = CGFloat((rgbValue) & obaque) / 255.0
+    convenience init(rgbValue: UInt32) {
+        let byte: UInt32 = 0xff
+        let red = CGFloat((rgbValue >> 16) & byte) / 255.0
+        let green = CGFloat((rgbValue >> 8) & byte) / 255.0
+        let blue = CGFloat((rgbValue) & byte) / 255.0
 
-    self.init(red: red, green: green, blue: blue, alpha: 1.0)
-  }
+        self.init(red: red, green: green, blue: blue, alpha: 1.0)
+    }
 }
