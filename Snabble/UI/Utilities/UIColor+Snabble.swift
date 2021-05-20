6ab0d0fda374c45bf226a9d5919a1f648681e60f
Gereon Steffens <gereon@snabble.io>
2021-05-20 13:18:20 +0200

    fix color for dark mode

--- Snabble/UI/Utilities/UIColor+Snabble.swift
+++ Snabble/UI/Utilities/UIColor+Snabble.swift
@@ -9,5 +9,18 @@ import UIKit
 
 extension UIColor {
     static var shadowColor = UIColor(rgbaValue: 0x2222223f)
-    static var borderColor = UIColor(rgbaValue: 0x00000019)
+
+    static var borderColor: UIColor {
+        guard #available(iOS 13.0, *) else {
+            return UIColor(rgbaValue: 0x00000019)
+        }
+
+        return UIColor { traitCollection in
+            if traitCollection.userInterfaceStyle == .dark {
+                return UIColor(rgbaValue: 0xFFFFFF19)
+            }
+
+            return UIColor(rgbaValue: 0x00000019)
+        }
+    }
 }
