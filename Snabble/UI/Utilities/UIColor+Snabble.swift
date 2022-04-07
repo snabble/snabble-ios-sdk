422391038215b970f6f63d215dfd30be0d61e83b
Andreas Osberghaus <andreas.osberghaus@snabble.io>
2022-04-07 12:12:27 +0200

    add rgba information as comment

--- Snabble/UI/Utilities/UIColor+Snabble.swift
+++ Snabble/UI/Utilities/UIColor+Snabble.swift
@@ -8,19 +8,19 @@ import Foundation
 import UIKit
 
 extension UIColor {
-    static var shadowColor = UIColor(rgbaValue: 0x2222223f)
+    static var shadowColor = UIColor(rgbaValue: 0x2222223f) // rgba(34, 34, 34, 0.2470588235)
 
     static var borderColor: UIColor {
         guard #available(iOS 13.0, *) else {
-            return UIColor(rgbaValue: 0x00000019)
+            return UIColor(rgbaValue: 0x00000019) // rgba(0, 0, 0, 0.09803921569)
         }
 
         return UIColor { traitCollection in
             if traitCollection.userInterfaceStyle == .dark {
-                return UIColor(rgbaValue: 0xFFFFFF19)
+                return UIColor(rgbaValue: 0xFFFFFF19) // rgba(1, 1, 1, 0.09803921569)
             }
 
-            return UIColor(rgbaValue: 0x00000019)
+            return UIColor(rgbaValue: 0x00000019) // rgba(0, 0, 0, 0.09803921569)
         }
     }
 }
