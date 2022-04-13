049e70d243ea842489f0372c8dccf321c62afca3
Andreas Osberghaus <andreas.osberghaus@snabble.io>
2022-04-13 13:55:14 +0200

    update error message

--- Snabble/Core/Cart/ShoppingCart.swift
+++ Snabble/Core/Cart/ShoppingCart.swift
@@ -414,7 +414,7 @@ extension ShoppingCart {
                 try move(from: oldDirectory, to: directory)
                 try delete(in: oldDirectory)
             } catch {
-                print(error)
+                print("Error: Could not delete \(cartURL(in: oldDirectory)). Details:"error)
             }
         }
 
