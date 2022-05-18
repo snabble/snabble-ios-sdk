74fbb4d7c69b6deef06f288bfecd55e3e5279968
Anastasia Mishur <40164951+mobileapps23@users.noreply.github.com>
2022-05-18 15:37:15 +0300

    remove StyleKey

--- Snabble/UI/Utilities/DynamicFont.swift
+++ Snabble/UI/Utilities/DynamicFont.swift
@@ -8,47 +8,10 @@ import UIKit
 
 // TODO: Need refactoring before creating public interface
 
-// First we declare list of Styles as they are listed at Confluence, that are using in the SDK
-// (maybe change it to a struct with variables, or implement as a protocol)
-enum StyleKey: String {
-        case largeTitle, title1, title2, title3
-        case headline, subheadline, body
-        case footnote, caption1, caption2
-}
-
-// Then linking our StyleKeys with system TextStyles, cause we are not using all system styles (for example '.callout')
-extension UIFont.TextStyle {
-    init (_ styleKey: StyleKey) {
-        switch styleKey {
-        case .largeTitle: self = .largeTitle
-        case .title1: self = .title1
-        case .title2: self = .title2
-        case .title3: self = .title3
-        case .headline: self = .headline
-        case .subheadline: self = .subheadline
-        case .body: self = .body
-        case .footnote: self = .footnote
-        case .caption1: self = .caption1
-        case .caption2: self = .caption2
-        }
-    }
-}
-
-// Also weight could be linked with StyleKey, and this will allow to remove weight as a parameter for methods below https://developer.apple.com/design/human-interface-guidelines/ios/visual-design/typography
-extension UIFont.Weight {
-    init (_ styleKey: StyleKey) {
-        switch styleKey {
-        case .headline: self = .semibold
-        default: self = .regular
-        }
-    }
-}
-
 extension UIFont {
-    static func preferredFont(forTextStyle textStyle: StyleKey, weight: UIFont.Weight = .regular) -> UIFont {
-        let style = UIFont.TextStyle(textStyle)
+    static func preferredFont(forTextStyle textStyle: UIFont.TextStyle, weight: UIFont.Weight) -> UIFont {
         let descriptor = UIFontDescriptor
-            .preferredFontDescriptor(withTextStyle: style)
+            .preferredFontDescriptor(withTextStyle: textStyle)
             .addingAttributes([
                 .traits: [ UIFontDescriptor.TraitKey.weight: weight ]
             ])
@@ -57,14 +20,14 @@ extension UIFont {
 }
 
 extension UILabel {
-    func useDynamicFont(forTextStyle textStyle: StyleKey) {
+    func useDynamicFont(forTextStyle textStyle: UIFont.TextStyle) {
         font = .preferredFont(forTextStyle: textStyle)
         adjustsFontForContentSizeCategory = true
         numberOfLines = 0
         lineBreakMode = .byWordWrapping
     }
 
-    func useDynamicFont(forTextStyle textStyle: StyleKey, weight: UIFont.Weight) {
+    func useDynamicFont(forTextStyle textStyle: UIFont.TextStyle, weight: UIFont.Weight) {
         font = .preferredFont(forTextStyle: textStyle, weight: weight)
         adjustsFontForContentSizeCategory = true
         numberOfLines = 0
@@ -73,35 +36,35 @@ extension UILabel {
 }
 
 extension UITextView {
-    func useDynamicFont(forTextStyle textStyle: StyleKey) {
+    func useDynamicFont(forTextStyle textStyle: UIFont.TextStyle) {
         font = .preferredFont(forTextStyle: textStyle)
         adjustsFontForContentSizeCategory = true
     }
 
-    func useDynamicFont(forTextStyle textStyle: StyleKey, weight: UIFont.Weight) {
+    func useDynamicFont(forTextStyle textStyle: UIFont.TextStyle, weight: UIFont.Weight) {
         font = .preferredFont(forTextStyle: textStyle, weight: weight)
         adjustsFontForContentSizeCategory = true
     }
 }
 
 extension UITextField {
-    func useDynamicFont(forTextStyle textStyle: StyleKey) {
+    func useDynamicFont(forTextStyle textStyle: UIFont.TextStyle) {
         font = .preferredFont(forTextStyle: textStyle)
         adjustsFontForContentSizeCategory = true
     }
 
-    func useDynamicFont(forTextStyle textStyle: StyleKey, weight: UIFont.Weight) {
+    func useDynamicFont(forTextStyle textStyle: UIFont.TextStyle, weight: UIFont.Weight) {
         font = .preferredFont(forTextStyle: textStyle, weight: weight)
         adjustsFontForContentSizeCategory = true
     }
 }
 
 extension UIButton {
-    func useDynamicFont(forTextStyle textStyle: StyleKey) {
+    func useDynamicFont(forTextStyle textStyle: UIFont.TextStyle) {
         titleLabel?.useDynamicFont(forTextStyle: textStyle)
     }
 
-    func useDynamicFont(forTextStyle textStyle: StyleKey, weight: UIFont.Weight = .regular) {
+    func useDynamicFont(forTextStyle textStyle: UIFont.TextStyle, weight: UIFont.Weight = .regular) {
         titleLabel?.useDynamicFont(forTextStyle: textStyle, weight: weight)
     }
 }
