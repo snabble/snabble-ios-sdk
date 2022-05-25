fece6f9c9271cac6a46305db07b806e71b1c385e
Anastasia Mishur <40164951+mobileapps23@users.noreply.github.com>
2022-05-25 18:01:36 +0300

    SupervisorRejectedViewController refactored for dynamic content

--- Snabble/UI/Payment/Checks/SupervisorRejectedViewController.swift
+++ Snabble/UI/Payment/Checks/SupervisorRejectedViewController.swift
@@ -20,6 +20,101 @@ final class SupervisorRejectedViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
 
+    override public func loadView() {
+        let contentView = UIView(frame: UIScreen.main.bounds)
+        contentView.restrictDynamicTypeSize(from: nil, to: .extraExtraExtraLarge)
+
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.backgroundColor = .systemBackground
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.alwaysBounceVertical = false
+
+        let contentLayoutGuide = scrollView.contentLayoutGuide
+        let frameLayoutGuide = scrollView.frameLayoutGuide
+
+        let wrapperView = UIView()
+        wrapperView.translatesAutoresizingMaskIntoConstraints = false
+
+        let stackViewLayout = UILayoutGuide()
+
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
+        stackView.alignment = .center
+        stackView.spacing = 32
+
+        let topIcon = UIImageView(image: Asset.SnabbleSDK.iconHintBig.image.withRenderingMode(.alwaysTemplate))
+        topIcon.translatesAutoresizingMaskIntoConstraints = false
+        topIcon.contentMode = .scaleAspectFit
+        topIcon.tintColor = .label
+        topIcon.adjustsImageSizeForAccessibilityContentSizeCategory = true
+        topIcon.setContentHuggingPriority(.defaultLow + 1, for: .horizontal)
+        topIcon.setContentHuggingPriority(.defaultLow + 2, for: .vertical)
+
+
+
+        let messageLabel = UILabel()
+        messageLabel.translatesAutoresizingMaskIntoConstraints = false
+        messageLabel.textColor = .label
+        messageLabel.useDynamicFont(forTextStyle: .body)
+        messageLabel.textAlignment = .center
+        messageLabel.setContentHuggingPriority(.defaultLow + 1, for: .horizontal)
+        messageLabel.setContentHuggingPriority(.defaultLow + 1, for: .vertical)
+        messageLabel.text = L10n.Snabble.Payment.rejectedHint
+
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.makeSnabbleButton()
+        button.useDynamicFont(forTextStyle: .headline)
+        button.setTitle(SnabbleSDK.L10n.Snabble.Payment.backToCart, for: .normal)
+        button.addTarget(self, action: #selector(backButtonTapped(_:)), for: .touchUpInside)
+
+
+        contentView.addSubview(scrollView)
+        scrollView.addSubview(wrapperView)
+
+        wrapperView.addLayoutGuide(stackViewLayout)
+        wrapperView.addSubview(stackView)
+        wrapperView.addSubview(button)
+
+        stackView.addArrangedSubview(topIcon)
+        stackView.addArrangedSubview(messageLabel)
+
+        NSLayoutConstraint.activate([
+            frameLayoutGuide.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            frameLayoutGuide.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            frameLayoutGuide.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor),
+            frameLayoutGuide.bottomAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.bottomAnchor),
+            frameLayoutGuide.widthAnchor.constraint(equalTo: contentLayoutGuide.widthAnchor),
+
+            contentLayoutGuide.leadingAnchor.constraint(equalTo: wrapperView.leadingAnchor),
+            contentLayoutGuide.topAnchor.constraint(equalTo: wrapperView.topAnchor),
+            contentLayoutGuide.trailingAnchor.constraint(equalTo: wrapperView.trailingAnchor),
+            contentLayoutGuide.bottomAnchor.constraint(equalTo: wrapperView.bottomAnchor),
+            wrapperView.heightAnchor.constraint(greaterThanOrEqualTo: frameLayoutGuide.heightAnchor),
+
+            button.heightAnchor.constraint(equalToConstant: 48),
+            button.bottomAnchor.constraint(equalTo: wrapperView.bottomAnchor, constant: -16),
+            button.leadingAnchor.constraint(equalTo: wrapperView.leadingAnchor, constant: 16),
+            button.trailingAnchor.constraint(equalTo: wrapperView.trailingAnchor, constant: -16),
+
+            stackViewLayout.topAnchor.constraint(equalTo: wrapperView.topAnchor),
+            stackViewLayout.leadingAnchor.constraint(equalTo: wrapperView.leadingAnchor, constant: 24),
+            stackViewLayout.trailingAnchor.constraint(equalTo: wrapperView.trailingAnchor, constant: -24),
+            stackViewLayout.bottomAnchor.constraint(equalTo: button.topAnchor),
+
+            stackView.leadingAnchor.constraint(equalTo: stackViewLayout.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: stackViewLayout.trailingAnchor),
+            stackView.centerYAnchor.constraint(equalTo: stackViewLayout.centerYAnchor),
+            stackView.topAnchor.constraint(greaterThanOrEqualTo: stackViewLayout.topAnchor),
+            stackView.bottomAnchor.constraint(lessThanOrEqualTo: stackViewLayout.bottomAnchor)
+        ])
+
+        self.view = contentView
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -27,54 +122,6 @@ final class SupervisorRejectedViewController: UIViewController {
         self.navigationItem.hidesBackButton = true
 
         view.backgroundColor = .systemBackground
-
-        let topSpace = UIView()
-        topSpace.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(topSpace)
-
-        let button = UIButton(type: .system)
-        button.makeSnabbleButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle(SnabbleSDK.L10n.Snabble.Payment.backToCart, for: .normal)
-        button.addTarget(self, action: #selector(backButtonTapped(_:)), for: .touchUpInside)
-        view.addSubview(button)
-
-        NSLayoutConstraint.activate([
-            topSpace.topAnchor.constraint(equalTo: view.topAnchor),
-            topSpace.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            topSpace.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            topSpace.bottomAnchor.constraint(equalTo: button.topAnchor),
-
-            button.heightAnchor.constraint(equalToConstant: 48),
-            button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16),
-            button.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
-        ])
-
-        let stack = UIStackView()
-        stack.translatesAutoresizingMaskIntoConstraints = false
-        stack.axis = .vertical
-        stack.spacing = 32
-
-        let hintIcon = UIImageView(image: Asset.SnabbleSDK.iconHintBig.image.withRenderingMode(.alwaysTemplate))
-        hintIcon.contentMode = .scaleAspectFit
-        hintIcon.tintColor = .label
-        stack.addArrangedSubview(hintIcon)
-
-        let label = UILabel()
-        label.numberOfLines = 0
-        label.textAlignment = .center
-        label.text = L10n.Snabble.Payment.rejectedHint
-
-        stack.addArrangedSubview(label)
-        topSpace.addSubview(stack)
-
-        NSLayoutConstraint.activate([
-            stack.centerXAnchor.constraint(equalTo: topSpace.centerXAnchor),
-            stack.centerYAnchor.constraint(equalTo: topSpace.centerYAnchor),
-            stack.leadingAnchor.constraint(equalTo: topSpace.leadingAnchor, constant: 24),
-            stack.trailingAnchor.constraint(equalTo: topSpace.trailingAnchor, constant: -24)
-        ])
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -86,7 +133,7 @@ final class SupervisorRejectedViewController: UIViewController {
         delegate?.track(.checkoutRejected)
     }
 
-    @IBAction private func backButtonTapped(_ sender: UIButton) {
+    @objc private func backButtonTapped(_ sender: UIButton) {
         guard let viewControllers = self.navigationController?.viewControllers, viewControllers.count > 2 else {
             return
         }
