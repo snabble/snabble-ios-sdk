3bab7f778917aa2aa4b252f90a7f15a551a15466
Andreas Osberghaus <andreas.osberghaus@snabble.io>
2022-05-31 14:03:23 +0200

    add available to method

--- Snabble/UI/Utilities/UIView+RestrictDynamicTypeSize.swift
+++ Snabble/UI/Utilities/UIView+RestrictDynamicTypeSize.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 extension UIView {
+    @available(iOS 15, *)
     func restrictDynamicTypeSize(from min: UIContentSizeCategory?, to max: UIContentSizeCategory?) {
         self.minimumContentSizeCategory = min
         self.maximumContentSizeCategory = max
