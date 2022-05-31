d73645f078991839020b8be14c4997d66b85198d
Andreas Osberghaus <andreas.osberghaus@snabble.io>
2022-05-31 14:58:14 +0200

    add quantityview

--- Snabble/UI/ShoppingCart/ShoppingCartTableCellView.swift
+++ Snabble/UI/ShoppingCart/ShoppingCartTableCellView.swift
@@ -10,6 +10,74 @@ protocol ShoppingCardCellViewDelegate: AnyObject {
     func minusButtonTapped()
 }
 
+final class QuantityView: UIView {
+    private(set) weak var minusButton: UIButton?
+    private(set) weak var plusButton: UIButton?
+    private(set) weak var quantityLabel: UILabel?
+
+    override init(frame: CGRect) {
+        let minusButton = UIButton(type: .custom)
+        minusButton.translatesAutoresizingMaskIntoConstraints = false
+        minusButton.setImage(Asset.SnabbleSDK.iconMinus.image, for: .normal)
+        minusButton.makeBorderedButton()
+        minusButton.backgroundColor = .secondarySystemBackground
+
+        let plusButton = UIButton(type: .custom)
+        plusButton.translatesAutoresizingMaskIntoConstraints = false
+        plusButton.setImage(Asset.SnabbleSDK.iconPlus.image, for: .normal)
+        plusButton.makeBorderedButton()
+        plusButton.backgroundColor = .secondarySystemBackground
+
+        let quantityLabel = UILabel()
+        quantityLabel.translatesAutoresizingMaskIntoConstraints = false
+        quantityLabel.font = UIFont.systemFont(ofSize: 13, weight: .semibold)
+        quantityLabel.textColor = .label
+        quantityLabel.textAlignment = .center
+        quantityLabel.setContentCompressionResistancePriority(.defaultHigh + 1, for: .horizontal)
+        super.init(frame: frame)
+
+        addSubview(minusButton)
+        addSubview(plusButton)
+        addSubview(quantityLabel)
+
+        self.minusButton = minusButton
+        self.plusButton = plusButton
+        self.quantityLabel = quantityLabel
+
+        NSLayoutConstraint.activate([
+            minusButton.topAnchor.constraint(greaterThanOrEqualTo: topAnchor),
+            bottomAnchor.constraint(greaterThanOrEqualTo: minusButton.bottomAnchor),
+            minusButton.centerYAnchor.constraint(equalTo: centerYAnchor),
+
+            plusButton.topAnchor.constraint(greaterThanOrEqualTo: topAnchor),
+            bottomAnchor.constraint(greaterThanOrEqualTo: plusButton.bottomAnchor),
+            plusButton.centerYAnchor.constraint(equalTo: centerYAnchor),
+
+            quantityLabel.topAnchor.constraint(greaterThanOrEqualTo: topAnchor),
+            bottomAnchor.constraint(greaterThanOrEqualTo: quantityLabel.bottomAnchor),
+            quantityLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+
+            minusButton.leadingAnchor.constraint(equalTo: leadingAnchor),
+            quantityLabel.leadingAnchor.constraint(equalTo: minusButton.trailingAnchor),
+            plusButton.leadingAnchor.constraint(equalTo: quantityLabel.trailingAnchor),
+            trailingAnchor.constraint(equalTo: plusButton.trailingAnchor),
+
+            minusButton.widthAnchor.constraint(equalToConstant: 32),
+            minusButton.heightAnchor.constraint(equalToConstant: 32),
+
+            plusButton.widthAnchor.constraint(equalTo: minusButton.widthAnchor),
+            plusButton.heightAnchor.constraint(equalTo: minusButton.heightAnchor),
+
+            quantityLabel.widthAnchor.constraint(greaterThanOrEqualToConstant: 34),
+            quantityLabel.widthAnchor.constraint(equalToConstant: 34).usingPriority(.defaultHigh)
+        ])
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
 final class ShoppingCartTableCellView: UIView {
     private weak var leftStackView: UIStackView?
     private weak var centerStackView: UIStackView?
@@ -35,10 +103,7 @@ final class ShoppingCartTableCellView: UIView {
     public weak var quantityInput: UITextField?
     public weak var unitsLabel: UILabel!
 
-    public weak var buttonWrapper: UIView?
-    public weak var minusButton: UIButton?
-    public weak var plusButton: UIButton?
-    public weak var quantityLabel: UILabel?
+    public weak var quantityView: QuantityView?
 
     private var units: UILabel {
         let label = UILabel()
@@ -109,13 +174,20 @@ final class ShoppingCartTableCellView: UIView {
     private func setupUI() {
         let leftStackView = self.stackView
         leftStackView.axis = .horizontal
-        leftStackView.setContentHuggingPriority(.defaultLow + 2, for: .horizontal)
+        leftStackView.setContentHuggingPriority(.required, for: .horizontal)
+
+        let centerStackView = self.stackView
+        centerStackView.axis = .vertical
+
+        let rightStackView = self.stackView
+        rightStackView.axis = .horizontal
+        rightStackView.setContentHuggingPriority(.required, for: .horizontal)
 
         let badgeWrapper = self.customView
         let badgeLabel = self.badge
         badgeLabel.setContentHuggingPriority(.defaultHigh, for: .horizontal)
-        let imageWrapper = self.customView
 
+        let imageWrapper = self.customView
         let imageBadge = self.badge
 
         let imageBackground = self.customView
@@ -140,24 +212,16 @@ final class ShoppingCartTableCellView: UIView {
             spinner.style = .gray
         }
 
-        let centerStackView = self.stackView
-        centerStackView.axis = .vertical
-
         let nameLabel = self.customLabel
         nameLabel.font = UIFont.systemFont(ofSize: 15)
         nameLabel.textColor = .label
         nameLabel.textAlignment = .natural
         nameLabel.numberOfLines = 0
-        nameLabel.setContentHuggingPriority(.defaultLow + 2, for: .vertical)
 
         let priceLabel = self.customLabel
         priceLabel.font = UIFont.monospacedDigitSystemFont(ofSize: 13, weight: .regular)
         priceLabel.textColor = .systemGray
         priceLabel.textAlignment = .natural
-
-        let rightStackView = self.stackView
-        rightStackView.axis = .horizontal
-        rightStackView.setContentHuggingPriority(.defaultLow + 2, for: .horizontal)
 
         let weightWrapper = self.customView
         let weightLabel = self.customLabel
@@ -179,24 +243,10 @@ final class ShoppingCartTableCellView: UIView {
         quantityInput.adjustsFontSizeToFitWidth = true
 
         let unitsLabel = self.units
-        let buttonWrapper = self.customView
 
-        let minusButton = self.button
-        minusButton.makeBorderedButton()
-        minusButton.backgroundColor = .systemBackground
-        minusButton.setImage(Asset.SnabbleSDK.iconMinus.image, for: .normal)
-        minusButton.addTarget(self, action: #selector(minusButtonTapped(_:)), for: .touchUpInside)
-
-        let quantityLabel = self.customLabel
-        quantityLabel.font = UIFont.systemFont(ofSize: 13, weight: .semibold)
-        quantityLabel.textColor = .label
-        quantityLabel.textAlignment = .center
-
-        let plusButton = self.button
-        plusButton.makeBorderedButton()
-        plusButton.backgroundColor = .systemBackground
-        plusButton.setImage(Asset.SnabbleSDK.iconPlus.image, for: .normal)
-        plusButton.addTarget(self, action: #selector(plusButtonTapped(_:)), for: .touchUpInside)
+        let quantityView = QuantityView(frame: .zero)
+        quantityView.plusButton?.addTarget(self, action: #selector(plusButtonTapped(_:)), for: .touchUpInside)
+        quantityView.minusButton?.addTarget(self, action: #selector(minusButtonTapped(_:)), for: .touchUpInside)
 
         addSubview(leftStackView)
         addSubview(centerStackView)
@@ -210,7 +260,7 @@ final class ShoppingCartTableCellView: UIView {
 
         rightStackView.addArrangedSubview(weightWrapper)
         rightStackView.addArrangedSubview(entryWrapper)
-        rightStackView.addArrangedSubview(buttonWrapper)
+        rightStackView.addArrangedSubview(quantityView)
 
         badgeWrapper.addSubview(badgeLabel)
 
@@ -224,10 +274,6 @@ final class ShoppingCartTableCellView: UIView {
 
         entryWrapper.addSubview(quantityInput)
         entryWrapper.addSubview(unitsLabel)
-
-        buttonWrapper.addSubview(minusButton)
-        buttonWrapper.addSubview(quantityLabel)
-        buttonWrapper.addSubview(plusButton)
 
         self.leftStackView = leftStackView
         self.badgeWrapper = badgeWrapper
@@ -247,20 +293,18 @@ final class ShoppingCartTableCellView: UIView {
         self.entryWrapper = entryWrapper
         self.quantityInput = quantityInput
         self.unitsLabel = unitsLabel
-        self.buttonWrapper = buttonWrapper
-        self.minusButton = minusButton
-        self.quantityLabel = quantityLabel
-        self.plusButton = plusButton
+        self.quantityView = quantityView
 
         NSLayoutConstraint.activate([
-            leftStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
             leftStackView.topAnchor.constraint(equalTo: topAnchor),
             leftStackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+
+            leftStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
             leftStackView.trailingAnchor.constraint(equalTo: centerStackView.leadingAnchor),
 
             badgeLabel.leadingAnchor.constraint(equalTo: leftStackView.leadingAnchor),
             badgeLabel.topAnchor.constraint(equalTo: badgeWrapper.topAnchor, constant: 11),
-            badgeLabel.trailingAnchor.constraint(equalTo: badgeWrapper.trailingAnchor, constant: -8),
+            badgeLabel.trailingAnchor.constraint(equalTo: badgeWrapper.trailingAnchor),
 
             imageBackground.leadingAnchor.constraint(equalTo: imageWrapper.leadingAnchor, constant: 8),
             imageBackground.widthAnchor.constraint(equalToConstant: 32),
@@ -285,7 +329,7 @@ final class ShoppingCartTableCellView: UIView {
 
             centerStackView.trailingAnchor.constraint(equalTo: rightStackView.leadingAnchor),
             centerStackView.topAnchor.constraint(equalTo: topAnchor, constant: 8),
-            bottomAnchor.constraint(equalTo: centerStackView.bottomAnchor, constant: 8).usingPriority(.required),
+            bottomAnchor.constraint(equalTo: centerStackView.bottomAnchor, constant: 8),
             centerStackView.heightAnchor.constraint(greaterThanOrEqualToConstant: 32),
 
             trailingAnchor.constraint(equalTo: rightStackView.trailingAnchor, constant: 12),
@@ -307,21 +351,7 @@ final class ShoppingCartTableCellView: UIView {
             unitsLabel.leadingAnchor.constraint(equalTo: quantityInput.trailingAnchor),
             unitsLabel.widthAnchor.constraint(equalToConstant: 17),
             unitsLabel.trailingAnchor.constraint(equalTo: entryWrapper.trailingAnchor),
-            unitsLabel.centerYAnchor.constraint(equalTo: quantityInput.centerYAnchor),
-
-            minusButton.leadingAnchor.constraint(equalTo: buttonWrapper.leadingAnchor),
-            minusButton.widthAnchor.constraint(equalToConstant: 32),
-            minusButton.topAnchor.constraint(equalTo: buttonWrapper.topAnchor, constant: 8),
-            minusButton.heightAnchor.constraint(equalToConstant: 32),
-
-            quantityLabel.leadingAnchor.constraint(equalTo: minusButton.trailingAnchor),
-            quantityLabel.widthAnchor.constraint(equalToConstant: 34),
-            quantityLabel.centerYAnchor.constraint(equalTo: minusButton.centerYAnchor),
-            plusButton.leadingAnchor.constraint(equalTo: quantityLabel.trailingAnchor),
-            plusButton.widthAnchor.constraint(equalToConstant: 32),
-            plusButton.trailingAnchor.constraint(equalTo: buttonWrapper.trailingAnchor),
-            plusButton.heightAnchor.constraint(equalToConstant: 32),
-            plusButton.centerYAnchor.constraint(equalTo: minusButton.centerYAnchor)
+            unitsLabel.centerYAnchor.constraint(equalTo: quantityInput.centerYAnchor)
         ])
     }
 
@@ -348,7 +378,7 @@ final class ShoppingCartTableCellView: UIView {
     }
 
     public func updateQuantityText(withText text: String?) {
-        self.quantityLabel?.text = text
+        self.quantityView?.quantityLabel?.text = text
         self.weightLabel?.text = text
         self.quantityInput?.text = text
     }
@@ -376,13 +406,16 @@ final class ShoppingCartTableCellView: UIView {
     }
 
     public func updateRightDisplay(withMode mode: RightDisplay) {
-        UIView.performWithoutAnimation {
-            [self.buttonWrapper, self.weightWrapper, self.entryWrapper].forEach { $0?.isHidden = true }
+        UIView.performWithoutAnimation { [self] in
+            [quantityView, weightWrapper, entryWrapper].forEach { $0?.isHidden = true }
             switch mode {
             case .none: ()
-            case .buttons: self.buttonWrapper?.isHidden = false
-            case .weightDisplay: self.weightWrapper?.isHidden = false
-            case .weightEntry: self.entryWrapper?.isHidden = false
+            case .buttons:
+                quantityView?.isHidden = false
+            case .weightDisplay:
+                weightWrapper?.isHidden = false
+            case .weightEntry:
+                entryWrapper?.isHidden = false
             }
         }
     }
