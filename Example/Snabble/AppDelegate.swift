1a4cf387f441c32acbd3120771577fcf79fb3684
Andreas Osberghaus <andreas.osberghaus@snabble.io>
2022-08-22 14:03:59 +0200

    load snabble setup in app delegate

--- Example/Snabble/AppDelegate.swift
+++ Example/Snabble/AppDelegate.swift
@@ -17,28 +17,54 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         Asset.provider = self
 
+        setupAppearance()
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = LoadingViewController()
+        window?.makeKeyAndVisible()
+
+        // Show onboarding
+//        window?.rootViewController?.present(OnboardingViewController(), animated: false)
+
+        snabbleSetup()
+
+        return true
+    }
+
+    private func snabbleSetup() {
+        let APPID = "snabble-sdk-demo-app-oguh3x"
+        let APPSECRET = "2TKKEG5KXWY6DFOGTZKDUIBTNIRVCYKFZBY32FFRUUWIUAFEIBHQ===="
+        let apiConfig = SnabbleSDK.Config(appId: APPID, secret: APPSECRET)
+
+        Snabble.setup(config: apiConfig) { snabble in
+            // initial config parsed/loaded
+            guard let project = snabble.projects.first else {
+                fatalError("project initialization failed - make sure APPID and APPSECRET are valid")
+            }
+
+            // register the project with the UI components
+            SnabbleUI.register(project)
+            snabble.checkInManager.shop = project.shops.first
+
+            // initialize the product database for this project
+            let productProvider = snabble.productProvider(for: project)
+            productProvider.setup { [unowned self] _ in
+                transitionView(with: project.shops.first!)
+            }
+        }
+    }
+
+    private func transitionView(with shop: Shop) {
         let shopsNavi = UINavigationController(rootViewController: ShopsViewController())
         let accountNavi = UINavigationController(rootViewController: AccountViewController())
-
-        let homeVC = HomeViewController()
-
-        let homeNavi = UINavigationController(rootViewController: homeVC)
-        homeNavi.navigationBar.isOpaque = true
+        let homeNavi = UINavigationController(rootViewController: HomeViewController(shop: shop))
 
         let tabBarController = UITabBarController()
         tabBarController.viewControllers = [homeNavi, shopsNavi, accountNavi]
         tabBarController.selectedIndex = 0
 
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.backgroundColor = .systemBackground
         window?.rootViewController = tabBarController
-        window?.makeKeyAndVisible()
-
-        window?.rootViewController?.present(OnboardingViewController(), animated: false)
-
-        setupAppearance()
-
-        return true
     }
 
     private func setupAppearance() {
