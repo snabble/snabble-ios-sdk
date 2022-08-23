f3a196f5f5aabc9e58d4607a6720bbc6fd9b0887
Andreas Osberghaus <andreas.osberghaus@snabble.io>
2022-08-23 10:55:17 +0200

    move image in a viewBuilder

--- Snabble/UI/Onboarding/Views/OnboardingItemView.swift
+++ Snabble/UI/Onboarding/Views/OnboardingItemView.swift
@@ -56,20 +56,24 @@ struct OnboardingItemView: View {
             } else {
                 EmptyView()
             }
-            if let imageSource = item.imageSource {
-                if let image = item.image {
-                    image
-                        .resizable()
-                        .scaledToFit()
-                        .padding([.top], topPadding)
-                } else {
-                    Text(imageSource)
-                        .font(.system(size: 72))
-                        .padding([.top], topPadding)
-                }
+        }
+    }
+
+    @ViewBuilder
+    var image: some View {
+        if let imageSource = item.imageSource {
+            if let image = item.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+                    .padding([.top], topPadding)
             } else {
-                EmptyView()
+                Text(imageSource)
+                    .font(.system(size: 72))
+                    .padding([.top], topPadding)
             }
+        } else {
+            EmptyView()
         }
     }
 
@@ -136,6 +140,7 @@ struct OnboardingItemView: View {
     var body: some View {
         VStack(alignment: .center, spacing: 20) {
             title
+            image
             text
             footer
         }
