1eb16a4efec3d109b7015b29a533c9eca6062483
Uwe Tilemann <45751166+utilem@users.noreply.github.com>
2022-08-30 11:18:07 +0200

    OpeningHours: 12/24 hour support  (#167)
    * Added support for 12/24 hour user setting
    
    * added space to localized phrase string and start/end time
    
    * Added fallback for invalid date strings

--- Snabble/UI/ShopFinder/Model/OpeningHourViewModel.swift
+++ Snabble/UI/ShopFinder/Model/OpeningHourViewModel.swift
@@ -13,6 +13,69 @@ public protocol OpeningHourProvider {
     var hour: String? { get }
 }
 
+extension DateFormatter {
+    var usesAMPM: Bool {
+        return DateFormatter.dateFormat(fromTemplate: "j", options: 0, locale: NSLocale.current)?.contains("a") ?? false
+    }
+}
+
+extension String {
+    var dateFromServerTime: Date? {
+        let formatter = DateFormatter()
+
+        formatter.locale = Locale(identifier: "de_DE")
+        formatter.dateFormat = "HH:mm"
+
+        return formatter.date(from: self)
+    }
+}
+
+extension Date {
+    var localTimeString: String? {
+        let formatter = DateFormatter()
+
+        formatter.dateFormat = formatter.usesAMPM ? "hh:mm aa" : "HH:mm"
+
+        return formatter.string(from: self)
+    }
+}
+
+struct TimeRange: CustomStringConvertible {
+    let start: String?
+    let end: String?
+    let startDate: Date?
+    let endDate: Date?
+
+    init(start: String?, end: String?) {
+        self.start = start
+        self.end = end
+        self.startDate = start?.dateFromServerTime
+        self.endDate = end?.dateFromServerTime
+    }
+
+    var startString: String? {
+        return self.startDate?.localTimeString ?? self.start
+    }
+
+    var endString: String? {
+        return self.endDate?.localTimeString ?? self.end
+    }
+
+    var description: String {
+        let start = startString
+        let end = endString
+        
+        if let start = start, let end = end {
+            return start + " – " + end
+        } else if let start = start {
+            return Asset.localizedString(forKey: "Snabble.Shop.Detail.startTime") + " " + start    // "ab"
+        } else if let end = end {
+            return Asset.localizedString(forKey: "Snabble.Shop.Detail.endTime") + " " + end // "bis"
+        }
+        return ""
+    }
+}
+
 extension String {
     var localDayOfWeek: String {
         var calendar = Calendar(identifier: .gregorian)
@@ -42,7 +105,7 @@ public struct OpeningHourViewModel: Swift.Identifiable, OpeningHourProvider {
         
         self.hour = filteredSpecification
             .sorted(by: { $0.opens.prefix(2) < $1.opens.prefix(2) })
-            .map { "\($0.opens.prefix(5)) – \($0.closes.prefix(5))" }
+            .map { TimeRange(start: String($0.opens.prefix(5)), end: String($0.closes.prefix(5))).description }
             .joined(separator: "\n")
     }
 }
