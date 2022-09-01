cc4c7c09bfd6f8a65998680724f14c5786974f46
Andreas Osberghaus <andreas.osberghaus@snabble.io>
2022-09-01 12:44:07 +0200

    add all textStyles (#174)

--- Snabble/UI/Utilities/TextStyle.swift
+++ Snabble/UI/Utilities/TextStyle.swift
@@ -8,21 +8,44 @@
 import SwiftUI
 
 public enum TextStyle: String {
-    case body
-    case footnote
-    case headline
+    case largeTitle
     case title
+    case title1
+    case title2
+    case title3
+    case headline
+    case body
+    case callout
+    case subheadline
+    case footnote
+    case caption
+    case caption1
+    case caption2
 
     var font: Font {
         switch self {
-        case .body:
-            return .body
-        case .footnote:
-            return .footnote
+        case .largeTitle:
+            return .largeTitle
+        case .title, .title1:
+            return .title
+        case .title2:
+            return .title2
+        case .title3:
+            return .title3
         case .headline:
             return .headline
-        case .title:
-            return .title
+        case .body:
+            return .body
+        case .callout:
+            return .callout
+        case .subheadline:
+            return .subheadline
+        case .footnote:
+            return .footnote
+        case .caption, .caption1:
+            return .caption
+        case .caption2:
+            return .caption2
         }
     }
 }
