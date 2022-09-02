79cd773b14401dd699192eb50d809ed9a9b055f5
Andreas Osberghaus <andreas.osberghaus@snabble.io>
2022-09-02 13:34:02 +0200

    update accountViewController

--- Example/Snabble/AccountViewController.swift
+++ Example/Snabble/AccountViewController.swift
@@ -9,24 +9,11 @@ import Foundation
 import SnabbleSDK
 import SwiftUI
 
-public struct ProfileView: View {
-    @ObservedObject public var viewModel: DynamicViewModel
-
-    public var body: some View {
-        NavigationView {
-            WidgetContainer(viewModel: viewModel, widgets: viewModel.widgets)
-                .navigationBarTitle(Asset.localizedString(forKey: "Sample.Profile.title"), displayMode: .inline)
-        }
-        .navigationViewStyle(.stack)
-   }
-}
-
-final class AccountViewController: SnabbleSDK.DynamicViewController {
-
+final class AccountViewController: DynamicViewController {
     override init(viewModel: DynamicViewModel) {
         super.init(viewModel: viewModel)
 
-        title = NSLocalizedString("Sample.account", comment: "")
+        title = NSLocalizedString("Sample.Profile.title", comment: "")
         tabBarItem.image = UIImage(named: "Navigation/TabBar/profil-off")
         tabBarItem.selectedImage = UIImage(named: "Navigation/TabBar/profil-off")
     }
