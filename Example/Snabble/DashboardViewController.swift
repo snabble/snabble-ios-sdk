3e9f6c1ec14140772092e7af8f94767e4f992d0d
Andreas Osberghaus <andreas.osberghaus@snabble.io>
2022-09-13 08:58:24 +0200

    update widgets with authorizationstatus

--- Example/Snabble/DashboardViewController.swift
+++ Example/Snabble/DashboardViewController.swift
@@ -8,11 +8,13 @@
 
 import Foundation
 import SnabbleSDK
+import CoreLocation
 
 final class DashboardViewController: SnabbleSDK.DynamicViewController {
     
     override init(viewModel: DynamicViewModel) {
         super.init(viewModel: viewModel)
+        delegate = self
 
         title = NSLocalizedString("Sample.home", comment: "")
 
@@ -22,5 +24,16 @@ final class DashboardViewController: SnabbleSDK.DynamicViewController {
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension DashboardViewController: SnabbleSDK.DynamicViewControllerDelegate {
+    func dynamicStackViewController(_ viewController: DynamicViewController, tappedWidget widget: Widget, userInfo: [String: Any]?) {
+        switch widget.id {
+        case "Snabble.LocationPermission.notDetermined":
+            CLLocationManager().requestWhenInUseAuthorization()
+        default:
+            break
+        }
     }
 }
--- Snabble/UI/DynamicView/WidgetLocationPermissionView.swift
+++ Snabble/UI/DynamicView/WidgetLocationPermissionView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 import CoreLocation
 
 public class LocationPermissionViewModel: NSObject, ObservableObject {
-    public var widgets: [Widget] = []
+    @Published public private(set) var widgets: [Widget] = []
 
     private let locationManager: CLLocationManager
 
@@ -21,7 +21,30 @@ public class LocationPermissionViewModel: NSObject, ObservableObject {
     }
 
     private func update(with status: CLAuthorizationStatus) {
-        print(#function, status.rawValue)
+        switch status {
+        case .authorizedAlways, .authorizedWhenInUse:
+            widgets = []
+        case .notDetermined:
+            widgets = [
+                WidgetButton(
+                    id: "Snabble.LocationPermission.notDetermined",
+                    text: "Standort freigeben",
+                    foregroundColorSource: "onAccent",
+                    backgroundColorSource: "accent"
+                )
+            ]
+        case .denied, .restricted:
+            widgets = [
+                WidgetButton(
+                    id: "Snabble.LocationPermission.denied-restricted",
+                    text: "Berechtigung fehlt",
+                    foregroundColorSource: "onAccent",
+                    backgroundColorSource: "accent"
+                )
+            ]
+        @unknown default:
+            widgets = []
+        }
     }
 }
 
@@ -44,6 +67,15 @@ public struct WidgetLocationPermissionView: View {
     }
     
     public var body: some View {
-        Text("LocationPermission")
+        VStack {
+            ForEach(viewModel.widgets, id: \.id) { widget in
+                if let buttonWidget = widget as? WidgetButton {
+                    WidgetButtonView(
+                        widget: buttonWidget,
+                        viewModel: dynamicViewModel
+                    )
+                }
+            }
+        }
     }
 }
