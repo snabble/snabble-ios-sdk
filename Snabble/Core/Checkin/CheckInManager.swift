2c21b9cb06fd74cf6a32df33be4657a790f17fa4
Andreas Osberghaus <andreas.osberghaus@snabble.io>
2022-09-14 16:43:50 +0200

    improve spacing

--- Snabble/Core/Checkin/CheckInManager.swift
+++ Snabble/Core/Checkin/CheckInManager.swift
@@ -35,7 +35,7 @@ public class CheckInManager: NSObject {
     /// Current checked in `Shop`
     public var shop: Shop? {
         didSet {
-            shopPublisher.send(shop)
+            shopPublisher.send(nil)
             if let shop = oldValue {
                 checkedInAt = nil
                 delegate?.checkInManager(self, didCheckOutOf: shop)
