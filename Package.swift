161bc379cab82c60401e513c74ce436ca1bcff91
Andreas Osberghaus <andreas.osberghaus@snabble.io>
2022-09-23 13:39:12 +0200

    add snabbleSDK library

--- Package.swift
+++ Package.swift
@@ -10,7 +10,10 @@ let package = Package(
         .iOS(.v14)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "SnabbleSDK",
+            targets: ["SnabbleCore", "SnabbleUI"]
+        ),
         .library(
             name: "SnabbleCore",
             targets: ["SnabbleCore"]
