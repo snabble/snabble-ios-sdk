8116ad582cd891adc4a56d459cda97896a402b2f
Andreas Osberghaus <andreas.osberghaus@snabble.io>
2022-10-14 14:51:18 +0200

    move changelog

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -2,7 +2,7 @@ import Danger
 
 let danger = Danger()
 let allSourceFiles = danger.git.modifiedFiles + danger.git.createdFiles
-let changelogChanged = allSourceFiles.contains("documentation/Changelog.md")
+let changelogChanged = allSourceFiles.contains("CHANGELOG.md")
 
 if !changelogChanged {
   warn("No CHANGELOG entry added.")
