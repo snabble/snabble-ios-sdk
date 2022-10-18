873c0f665d6e0f33bdb7f9040438203a302018a6
Andreas Osberghaus <andreas.osberghaus@snabble.io>
2022-10-18 11:45:13 +0200

    add lint warning

--- Sources/Core/API/String+Hash.swift
+++ Sources/Core/API/String+Hash.swift
@@ -20,6 +20,9 @@ extension String {
         }
     }
 
+
+    
+
     public var sha1: String {
         let data = Data(self.utf8)
         var digest = [UInt8](repeating: 0, count: Int(CC_SHA1_DIGEST_LENGTH))
