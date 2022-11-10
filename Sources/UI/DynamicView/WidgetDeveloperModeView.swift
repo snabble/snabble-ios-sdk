e582d2752f557fd82ca107a2234177cc62fa0bf6
Andreas Osberghaus <andreas.osberghaus@snabble.io>
2022-11-10 14:37:43 +0100

    Update WidgetDeveloperModeView.swift

--- Sources/UI/DynamicView/WidgetDeveloperModeView.swift
+++ Sources/UI/DynamicView/WidgetDeveloperModeView.swift
@@ -5,11 +5,30 @@
 //
 
 import SwiftUI
+import Combine
+
+public class DeveloperModeViewModel: NSObject, ObservableObject {
+    @Published public private(set) var widgets: [Widget] = []
+    @Published public private(set) var isEnabled: Bool = false
+
+    override init() {
+        self.isEnabled = UserDefaults.standard.bool(forKey: "io.snabble.developerMode")
+    }
+}
 
 public struct WidgetDeveloperModeView: View {
     let widget: WidgetDeveloperMode
 
+    @ObservedObject private var viewModel: DeveloperModeViewModel
+
+    init(widget: WidgetDeveloperMode) {
+        self.widget = widget
+        self.viewModel = .init()
+    }
+
     public var body: some View {
+        
+
         Text("Entwickler Modus")
     }
 }
