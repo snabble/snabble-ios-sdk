ec1ba31845240e68bc491801c8bf3d5a1edeaeaa
Uwe Tilemann <uwe.tilemann@snabble.io>
2023-01-25 08:18:52 +0100

    cleanup whitespaces

--- Sources/UI/Checkout/CheckoutStepsViewController.swift
+++ Sources/UI/Checkout/CheckoutStepsViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import SnabbleCore
 
 final class CheckoutStepsViewController: UIViewController {
-    
+
     private(set) weak var tableView: UITableView?
     private(set) weak var headerView: CheckoutHeaderView?
     private(set) weak var doneButton: UIButton?
