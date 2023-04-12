03a61e7a46b32cbc37671d4f4c757a4a87a3b743
Uwe Tilemann <45751166+utilem@users.noreply.github.com>
2023-04-12 16:30:14 +0200

    Apps 792 (#247)
    * use SwiftUI for coupons
    
    * fixed linter issues
    
    * Updated Changelog
    
    * Updated Changelog
    
    * added loadImage() function
    
    * Added cart item for activated but not usable coupons
    
    * fixed lint issues
    
    * Updated Changelog
    
    ---------
    
    Co-authored-by: Andreas Osberghaus <148758+mrgrauel@users.noreply.github.com>

---
+++ Sources/UI/ShoppingCart/Views/CouponItemView.swift
@@ -0,0 +1,51 @@
+//
+//  CouponItemView.swift
+//  
+//
+//  Created by Uwe Tilemann on 12.04.23.
+//
+
+import Foundation
+import SwiftUI
+import SnabbleCore
+
+struct CouponItemView: View {
+    @ObservedObject var itemModel: CouponCartItemModel
+    let showImages: Bool
+    
+    init(itemModel: CouponCartItemModel, showImages: Bool = true) {
+        self.itemModel = itemModel
+        self.showImages = showImages
+    }
+
+    @ViewBuilder
+    var leftView: some View {
+        if showImages {
+            SwiftUI.Image(systemName: "percent")
+                .cartImageModifier(padding: 10)
+                .foregroundColor(itemModel.isRedeemed ? .primary : .secondary)
+        }
+    }
+
+    var body: some View {
+        HStack {
+            leftView
+            VStack(alignment: .center, spacing: 4) {
+                HStack(alignment: .top) {
+                    Spacer()
+                    Text(itemModel.cartCoupon.coupon.name)
+                    Asset.image(named: "discount-badge")
+                }
+
+                if itemModel.isRedeemed == false {
+                    Text(keyed: "Snabble.Coupon.notUsable")
+                        .multilineTextAlignment(.center)
+                        .font(.footnote)
+                        .foregroundColor(.secondaryLabel)
+                }
+           }
+            .cartInfo()
+        }
+        .listRowBackground(Color.clear)
+    }
+}
