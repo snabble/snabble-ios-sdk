1b78d05106192e86dbd04ad356c4415317ef0772
Andreas Osberghaus <andreas.osberghaus@snabble.io>
2023-08-24 10:30:10 +0200

    add isGrabAndGo method

---
+++ Sources/Core/Metadata/Shop+GrabAndGo.swift
@@ -0,0 +1,16 @@
+//
+//  Shop+GrabAndGo.swift
+//  
+//
+//  Created by Andreas Osberghaus on 2023-08-24.
+//
+
+/// extension for shop that adds Grab&Go-based stuff
+extension Shop {
+    public var isGrabAndGo: Bool {
+        guard let flag = external?["grabAndGoEnabled"] as? Bool else {
+            return false
+        }
+        return flag
+    }
+}
