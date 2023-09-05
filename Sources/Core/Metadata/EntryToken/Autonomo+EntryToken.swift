c18fa0ff4e30086cd3918250d865171465e7e033
Andreas Osberghaus <148758+mrgrauel@users.noreply.github.com>
2023-09-05 12:57:58 +0200

    add autonomosession request (#265)
    * add autonomosession request
    
    * implement autonomo session
    
    * change shopId to string
    
    * add entrytoken as a protocol
    
    * fixes protection levels

---
+++ Sources/Core/Metadata/EntryToken/Autonomo+EntryToken.swift
@@ -0,0 +1,49 @@
+//
+//  Autonomo.swift
+//
+//
+//  Created by Andreas Osberghaus on 2023-09-04.
+//
+
+import Foundation
+
+enum Autonomo {
+    struct Session: Codable {
+        public let id: String
+        public let entryToken: EntryToken
+    }
+
+    struct EntryToken: Codable, SnabbleCore.EntryToken {
+        public let value: String
+        public let validUntil: Date
+        public let refreshAfter: Date
+    }
+}
+
+private struct SessionRequest: Encodable {
+    let shopID: String
+    let paymentMethod: String
+    let paymentOrigin: String
+}
+
+extension Project {
+    func getAutonomoSession(for shop: Shop, paymentMethodDetail: PaymentMethodDetail, completion: @escaping (Result<Autonomo.Session, SnabbleError>) -> Void) {
+        let tokenRequest = SessionRequest(
+            shopID: shop.id.rawValue,
+            paymentMethod: paymentMethodDetail.rawMethod.rawValue,
+            paymentOrigin: paymentMethodDetail.encryptedData
+        )
+
+        guard let data = try? JSONEncoder().encode(tokenRequest) else {
+            return completion(.failure(SnabbleError.invalid))
+        }
+
+        self.request(.post, "/\(shop.projectId)/autonomo/sessions", body: data, timeout: 5) { request in
+            guard let request = request else {
+                return completion(.failure(SnabbleError.noRequest))
+            }
+
+            self.perform(request, completion)
+        }
+    }
+}
