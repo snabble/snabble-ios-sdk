2bf069323b469422c42c22c2b35c1f5f1f0a6268
Andreas Osberghaus <andreas.osberghaus@snabble.io>
2023-09-14 10:49:52 +0200

    fixes dismiss overlay

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [unreleased]
+## [0.39.1] - 2023-09-14
+
+### Fixed
+* Overlay cannot be dismissed after keyboard input #APPS-1056
 
 ## [0.39.0] - 2023-09-13
 
--- Sources/UI/Utilities/UIViewController+Overlay.swift
+++ Sources/UI/Utilities/UIViewController+Overlay.swift
@@ -13,10 +13,11 @@ extension UIViewController {
 
     private static let backgroundViewAssociation = ObjectAssociation<UIView>(policy: .OBJC_ASSOCIATION_ASSIGN)
     private static let overlayViewAssociation = ObjectAssociation<UIView>(policy: .OBJC_ASSOCIATION_ASSIGN)
+    private static let overlayViewControllerAssociation = ObjectAssociation<UIViewController>(policy: .OBJC_ASSOCIATION_ASSIGN)
 
     private var backgroundView: UIView? {
         get {
-            UIViewController.backgroundViewAssociation[self]
+            UIViewController.backgroundViewAssociation[self] ?? nil
         }
         set {
             UIViewController.backgroundViewAssociation[self] = newValue
@@ -25,14 +26,58 @@ extension UIViewController {
 
     private var overlayView: UIView? {
         get {
-            UIViewController.overlayViewAssociation[self]
+            UIViewController.overlayViewAssociation[self] ?? nil
         }
         set {
             UIViewController.overlayViewAssociation[self] = newValue
         }
     }
 
+    private var overlayViewController: UIViewController? {
+        get {
+            UIViewController.overlayViewControllerAssociation[self] ?? nil
+        }
+        set {
+            UIViewController.overlayViewControllerAssociation[self] = newValue
+        }
+    }
+
     private static let bottomConstraintIdentifier = "bottomOverlayConstraint"
+
+    public func showOverlay(with overlayViewController: UIViewController) {
+        let backgroundView = UIView(frame: view.bounds)
+        backgroundView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        backgroundView.backgroundColor = .label.withAlphaComponent(0.5)
+        view.addSubview(backgroundView)
+        self.backgroundView = backgroundView
+
+        let overlayView = overlayViewController.view!
+        addChild(overlayViewController)
+        overlayView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(overlayView)
+        self.overlayView = overlayView
+
+        let layoutGuide: UILayoutGuide = (self as? UINavigationController)?.topViewController?.view.safeAreaLayoutGuide ?? view.safeAreaLayoutGuide
+
+        NSLayoutConstraint.activate([
+            overlayView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 2),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: overlayView.trailingAnchor, multiplier: 2),
+
+            overlayView.topAnchor.constraint(greaterThanOrEqualToSystemSpacingBelow: view.topAnchor, multiplier: 2),
+            overlayView.topAnchor.constraint(equalTo: layoutGuide.bottomAnchor).usingPriority(.defaultLow - 1)
+        ])
+
+        view.layoutIfNeeded()
+        UIView.animate(withDuration: 0.3) { [self] in
+            NSLayoutConstraint.activate([
+                layoutGuide.bottomAnchor.constraint(equalToSystemSpacingBelow: overlayView.bottomAnchor, multiplier: 2)
+                    .usingIdentifier(Self.bottomConstraintIdentifier)
+            ])
+            view.layoutIfNeeded()
+        }
+        overlayViewController.didMove(toParent: self)
+        self.overlayViewController = overlayViewController
+    }
 
     public func showOverlay(with overlayView: UIView) {
         let backgroundView = UIView(frame: view.bounds)
@@ -65,57 +110,25 @@ extension UIViewController {
         }
     }
 
-    public func dismissOverlay() {
-        view.layoutIfNeeded()
-
-        UIView.animate(
-            withDuration: 0.3,
-            animations: { [weak self] in
-                self?.view.constraints.first(with: Self.bottomConstraintIdentifier)?.isActive = false
-                self?.view.layoutIfNeeded()
-            },
-            completion: { [weak self] _ in
-                self?.backgroundView?.removeFromSuperview()
-                self?.overlayView?.removeFromSuperview()
-            }
-        )
-    }
-
-    // MARK: - Window
-
-    private static let windowAssociation = ObjectAssociation<UIWindow>()
-
-    private var overlayWindow: UIWindow? {
-        get {
-            UIViewController.windowAssociation[self]
+    public func dismissOverlay(animated: Bool = true) {
+        if let viewController = overlayViewController {
+            viewController.willMove(toParent: nil)
+            viewController.view.removeFromSuperview()
+            viewController.removeFromParent()
+            backgroundView?.removeFromSuperview()
+        } else {
+            view.layoutIfNeeded()
+            UIView.animate(
+                withDuration: animated ? 0.3 : 0.0,
+                animations: { [weak self] in
+                    self?.view.constraints.first(with: Self.bottomConstraintIdentifier)?.isActive = false
+                    self?.view.layoutIfNeeded()
+                },
+                completion: { [weak self] _ in
+                    self?.backgroundView?.removeFromSuperview()
+                    self?.overlayView?.removeFromSuperview()
+                }
+            )
         }
-        set {
-            UIViewController.windowAssociation[self] = newValue
-        }
-    }
-
-    func showWindowOverlay(with overlayView: UIView) {
-        let viewController = UIViewController()
-
-        let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
-        overlayWindow = UIWindow(windowScene: windowScene!)
-        overlayWindow?.backgroundColor = .label.withAlphaComponent(0.5)
-        overlayWindow?.rootViewController = viewController
-        overlayWindow?.windowLevel = .normal
-        overlayWindow?.makeKeyAndVisible()
-
-        overlayView.translatesAutoresizingMaskIntoConstraints = false
-        viewController.view.addSubview(overlayView)
-
-        NSLayoutConstraint.activate([
-            overlayView.leadingAnchor.constraint(equalToSystemSpacingAfter: viewController.view.leadingAnchor, multiplier: 2),
-            viewController.view.trailingAnchor.constraint(equalToSystemSpacingAfter: overlayView.trailingAnchor, multiplier: 2),
-
-            overlayView.centerYAnchor.constraint(equalTo: viewController.view.centerYAnchor),
-            overlayView.centerXAnchor.constraint(equalTo: viewController.view.centerXAnchor),
-
-            overlayView.topAnchor.constraint(greaterThanOrEqualToSystemSpacingBelow: viewController.view.topAnchor, multiplier: 2),
-            viewController.view.bottomAnchor.constraint(greaterThanOrEqualToSystemSpacingBelow: overlayView.bottomAnchor, multiplier: 2)
-        ])
     }
 }
