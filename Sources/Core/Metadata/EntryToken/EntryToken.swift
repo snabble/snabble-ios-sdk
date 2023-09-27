8811b5247d2c37ff3717f3dc6645b7f68b7cb204
Andreas Osberghaus <andreas.osberghaus@snabble.io>
2023-09-27 09:48:30 +0200

    remove deprecated method

--- Sources/Core/Metadata/EntryToken/EntryToken.swift
+++ Sources/Core/Metadata/EntryToken/EntryToken.swift
@@ -12,16 +12,3 @@ public protocol EntryToken: Codable {
     var validUntil: Date { get }
     var refreshAfter: Date { get }
 }
-
-public extension Project {
-    func getEntryToken(for shopID: Identifier<Shop>, completion: @escaping (Result<SnabbleCore.EntryToken, SnabbleError>) -> Void) {
-        getWanzelEntryToken(for: shopID) { result in
-            switch result {
-            case .success(let token):
-                completion(.success(token))
-            case .failure(let error):
-                completion(.failure(error))
-            }
-        }
-    }
-}
