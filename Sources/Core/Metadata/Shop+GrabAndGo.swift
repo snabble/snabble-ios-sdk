1a2b0cf71dd084d56ee26c73816c2da24e22f73a
Uwe Tilemann <dehale@me.com>
2024-02-15 10:19:14 +0100

    use whitelist for shop acceptedPaymentMethods

--- Sources/Core/Metadata/Shop+GrabAndGo.swift
+++ Sources/Core/Metadata/Shop+GrabAndGo.swift
@@ -14,20 +14,24 @@ extension Shop {
         return flag
     }
 
-    public func excludePaymentMethod(_ method: RawPaymentMethod) -> Bool {
-        return isGrabAndGo && method == .giropayOneKlick
+    public func includePaymentMethod(_ method: RawPaymentMethod) -> Bool {
+        return acceptedPaymentMethods.first(where: { $0 == method }) != nil
     }
 
-    public var paymentsMethods: [RawPaymentMethod] {
-        return Snabble.shared.projects
-            .filter { $0.id == projectId }
-            .flatMap { $0.paymentMethods }
-            .filter { $0.visible && excludePaymentMethod($0) == false }
+    public var acceptedPaymentMethods: [RawPaymentMethod] {
+        if isGrabAndGo {
+            return [.creditCardVisa, .creditCardMastercard, .creditCardAmericanExpress, .externalBilling]
+        } else {
+            return Snabble.shared.projects
+                .filter { $0.id == projectId }
+                .flatMap { $0.paymentMethods }
+                .filter { $0.visible }
+        }
     }
-
+    
     public var hasAcceptedPaymentMethod: Bool {
         let details = PaymentMethodDetails.read().compactMap({ $0.rawMethod })
         
-        return !paymentsMethods.filter({ details.contains($0) }).isEmpty
+        return !acceptedPaymentMethods.filter({ details.contains($0) }).isEmpty
     }
 }
