0ee3ccd2c8b125ecf78ee0723b954074ce6589d3
Andreas Osberghaus <andreas.osberghaus@snabble.io>
2024-02-21 09:04:56 +0100

    update changelog

--- Package.swift
+++ Package.swift
@@ -35,7 +35,7 @@ let package = Package(
         .package(url: "https://github.com/marmelroy/Zip.git", from: "2.1.2"),
         .package(url: "https://github.com/datatrans/ios-sdk.git", from: "3.4.0"),
         .package(url: "https://github.com/sberrevoets/SDCAlertView.git", from: "12.0.3"),
-        .package(url: "https://github.com/devicekit/DeviceKit.git", from: "5.2.1"),
+        .package(url: "https://github.com/devicekit/DeviceKit.git", from: "5.2.2"),
         .package(url: "https://github.com/snabble/Pulley.git", from: "2.9.2"),
         .package(url: "https://github.com/chrs1885/WCAG-Colors.git", from: "1.0.0")
     ],
