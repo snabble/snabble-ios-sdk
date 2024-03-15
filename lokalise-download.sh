d46126cf0c107504aad5dbafe8edfba13c36d9a5
Andreas Osberghaus <andreas.osberghaus@proton.me>
2024-03-15 12:57:49 +0100

    Add Sample Strings to Lokalise

--- lokalise-download.sh
+++ lokalise-download.sh
@@ -6,18 +6,33 @@ alias lokalise-download="lokalise2 file --config lokalise.yml download --format 
 # Setting the environment variable LOKALISE2_TOKEN is necessary to execute this script.
 
 # Project specific parameters:
-PROJECT_ID=3931709465f04f20a1bc18.55914019
-RES_PATH=Sources/UI/Resources
+SDK_PROJECT_ID=3931709465f04f20a1bc18.55914019
+SAMPLE_PROJECT_ID=8964099365f434ac71f546.06213099
+SDK_PATH=Sources/UI/Resources
+SAMPLE_PATH=Example/Snabble
 DIRECTORY_PREFIX="%LANG_ISO%.lproj"
 
 lokalise2 \
     --token $LOKALISE2_TOKEN \
-    --project-id $PROJECT_ID \
+    --project-id $SDK_PROJECT_ID \
     file download \
     --format strings \
     --directory-prefix $DIRECTORY_PREFIX \
     --original-filenames=true \
-    --unzip-to $RES_PATH \
+    --unzip-to $SDK_PATH \
+    --include-comments=false \
+    --include-description=false \
+    --export-empty-as=base  \
+    --placeholder-format=ios
+
+lokalise2 \
+    --token $LOKALISE2_TOKEN \
+    --project-id $SAMPLE_PROJECT_ID \
+    file download \
+    --format strings \
+    --directory-prefix $DIRECTORY_PREFIX \
+    --original-filenames=true \
+    --unzip-to $SAMPLE_PATH \
     --include-comments=false \
     --include-description=false \
     --export-empty-as=base  \
