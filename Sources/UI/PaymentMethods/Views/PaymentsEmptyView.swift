847776678956d7608ff6b7c3c0a4f4f0c9254296
Uwe Tilemann <dehale@me.com>
2024-04-10 16:57:49 +0200

    add PaymentsEmptyView

--- Sources/UI/PaymentMethods/Views/PaymentsEmptyView.swift
+++ Sources/UI/PaymentMethods/Views/PaymentsEmptyView.swift
@@ -1,8 +1,35 @@
 //
-//  File.swift
+//  PaymentsEmptyView.swift
 //  
 //
 //  Created by Uwe Tilemann on 10.04.24.
 //
 
-import Foundation
+import SwiftUI
+
+public struct PaymentsEmptyView: View {
+
+    public init() { }
+
+    public var body: some View {
+        let emptyString = Asset.localizedString(forKey: "Snabble.Payment.EmptyState.message")
+
+        if emptyString != "Snabble.Payment.EmptyState.message" {
+            VStack(spacing: 32) {
+                if let image: SwiftUI.Image = Asset.image(named: "creditcard", domain: nil) {
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 72)
+                        .foregroundColor(.accentColor)
+                }
+                
+                Text(emptyString)
+                    .multilineTextAlignment(.center)
+                    .lineLimit(2)
+                    .padding([.leading, .trailing], 48)
+            }
+            .padding()
+        }
+    }
+}
