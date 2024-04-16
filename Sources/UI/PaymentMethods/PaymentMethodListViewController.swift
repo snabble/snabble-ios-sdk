f734b08db7907fcf46d4d74af3fda3c752dd1870
Uwe Tilemann <dehale@me.com>
2024-04-16 12:22:45 +0200

    add debug info

--- Sources/UI/PaymentMethods/PaymentMethodListViewController.swift
+++ Sources/UI/PaymentMethods/PaymentMethodListViewController.swift
@@ -18,7 +18,9 @@ public final class PaymentMethodListViewController: UITableViewController {
         self.analyticsDelegate = analyticsDelegate
         super.init(style: .insetGrouped)
     }
-
+    deinit {
+        print("deinit: PaymentMethodListViewController")
+    }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
