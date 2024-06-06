4014ed38f3886849df818cf9d8e3faa996e39c7b
Uwe Tilemann <uwe.tilemann@snabble.io>
2024-06-06 17:37:37 +0200

    Update ClientError for validationErrors

--- Network/Sources/ClientError.swift
+++ Network/Sources/ClientError.swift
@@ -8,14 +8,24 @@
 import Foundation
 
 public struct ClientError: Decodable {
-    public let type: String
-    public let message: String
+    public let type: String?
+    public let message: String?
+    public let validationErrors: [Validation]?
     
     private enum CodingKeys: CodingKey {
         case type
         case message
+        case validationErrors
     }
-    
+    public struct Validation: Decodable {
+        public let field: String
+        public let category: String
+        
+        enum CodingKeys: CodingKey {
+            case field
+            case category
+        }
+    }
     private enum ErrorKey: CodingKey {
         case error
     }
@@ -23,7 +33,8 @@ public struct ClientError: Decodable {
     public init(from decoder: Decoder) throws {
         let topLevelContainer = try decoder.container(keyedBy: ErrorKey.self)
         let container = try topLevelContainer.nestedContainer(keyedBy: CodingKeys.self, forKey: .error)
-        self.type = try container.decode(String.self, forKey: .type)
-        self.message = try container.decode(String.self, forKey: .message)
+        self.type = try container.decodeIfPresent(String.self, forKey: .type)
+        self.message = try container.decodeIfPresent(String.self, forKey: .message)
+        self.validationErrors = try container.decodeIfPresent([Validation].self, forKey: .validationErrors)
     }
 }
