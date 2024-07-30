f5feb8a4db9b14faa304d2823b5333a690cd746a
utilem <uwe.tilemann@snabble.io>
2024-07-30 15:13:27 +0200

    Create DebugHelpers.swift
    helps to print data as JSON

---
+++ Core/Sources/Utilities/DebugHelpers.swift
@@ -0,0 +1,32 @@
+//
+//  DebugHelpers.swift
+//  
+//
+//  Created by Uwe Tilemann on 29.07.24.
+//
+
+import Foundation
+
+extension Data {
+    func printAsJSON() {
+        if let theJSONData = try? JSONSerialization.jsonObject(with: self, options: []) as? NSDictionary {
+            var swiftDict: [String: Any] = [:]
+            for key in theJSONData.allKeys {
+                let stringKey = key as? String
+                if let key = stringKey, let keyValue = theJSONData.value(forKey: key) {
+                    swiftDict[key] = keyValue
+                }
+            }
+            swiftDict.printAsJSON()
+        }
+    }
+}
+
+extension Dictionary {
+    func printAsJSON() {
+        if let theJSONData = try? JSONSerialization.data(withJSONObject: self, options: .prettyPrinted),
+            let theJSONText = String(data: theJSONData, encoding: String.Encoding.ascii) {
+            print("\(theJSONText)")
+        }
+    }
+}
