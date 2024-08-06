fffa511c7690c3ccf38746905d70e98b0ab1738e
Uwe Tilemann <uwe.tilemann@snabble.io>
2024-08-06 13:18:50 +0200

    Update User.swift

--- User/Sources/User.swift
+++ User/Sources/User.swift
@@ -8,25 +8,31 @@
 import Foundation
 
 public struct User: Codable {
-    public let firstname: String?
-    public let lastname: String?
-    public let email: String?
-    public let street: String?
-    public let zip: String?
-    public let city: String?
-    public let country: String?
-    public let state: String?
+    public var firstname: String?
+    public var lastname: String?
+    public var email: String?
+    public var phone: String?
+    public var street: String?
+    public var zip: String?
+    public var city: String?
+    public var country: String?
+    public var state: String?
     
-    public init(firstname: String?, lastname: String?, email: String?, street: String?, zip: String?, city: String?, country: String?, state: String?) {
+    public init(firstname: String?, lastname: String?, email: String?, phone: String?, street: String?, zip: String?, city: String?, country: String?, state: String?) {
         self.firstname = firstname
         self.lastname = lastname
         self.email = email
+        self.phone = phone
         self.street = street
         self.zip = zip
         self.city = city
         self.country = country
         self.state = state
     }
+    public init() {
+        self.init(firstname: nil, lastname: nil, email: nil, phone: nil, street: nil, zip: nil, city: nil, country: nil, state: nil)
+    }
+    
 }
 
 extension User {
