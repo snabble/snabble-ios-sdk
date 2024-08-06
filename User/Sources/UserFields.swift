0846f2173424534059cd94664186206d6510b12b
Uwe Tilemann <uwe.tilemann@snabble.io>
2024-08-06 13:19:01 +0200

    Create UserFields.swift

---
+++ User/Sources/UserFields.swift
@@ -0,0 +1,102 @@
+//
+//  UserFields.swift
+//
+//
+//  Created by Uwe Tilemann on 02.08.24.
+//
+import Foundation
+
+import SwiftUI
+
+// import SnabbleCore
+import SnabbleAssetProviding
+
+public enum UserField: String, CaseIterable, Swift.Identifiable, Hashable {
+    public var id: Self { self }
+    
+    case firstName
+    case lastName
+    case email
+    case phone
+    case dateOfBirth
+    case street
+    case zip
+    case city
+    case country
+    case state
+}
+
+extension UserField {
+    public var prompt: LocalizedStringKey {
+        switch self {
+        case .firstName:
+            "Snabble.User.firstName"
+        case .lastName:
+            "Snabble.User.lastName"
+        case .email:
+            "Snabble.User.email"
+        case .phone:
+            "Snabble.User.phone"
+        case .dateOfBirth:
+            "Snabble.User.dateOfBirth"
+        case .street:
+            "Snabble.User.street"
+        case .zip:
+            "Snabble.User.zip"
+        case .city:
+            "Snabble.User.city"
+        case .country:
+            "Snabble.User.country"
+        case .state:
+            "Snabble.User.state"
+        }
+    }
+
+    public var contentType: UITextContentType? {
+        switch self {
+        case .firstName:
+            return .givenName
+        case .lastName:
+            return .familyName
+        case .email:
+            return .emailAddress
+        case .phone:
+            return .telephoneNumber
+        case .dateOfBirth:
+            if #available(iOS 17.0, *) {
+                return .birthdateDay
+            } else {
+                return nil
+            }
+        case .street:
+            return .streetAddressLine1
+        case .zip:
+            return .postalCode
+        case .city:
+            return .addressCity
+        case .country:
+            return .countryName
+        case .state:
+            return .addressState
+        }
+    }
+    
+    public var keyboardType: UIKeyboardType {
+        switch self {
+        case .firstName, .lastName, .dateOfBirth, .street, .city, .country, .state:
+                .default
+        case .email:
+                .emailAddress
+        case .phone:
+                .phonePad
+        case .zip:
+                .numberPad
+        }
+    }
+}
+
+extension UserField {
+    public static func fieldsWithout(_ unwanted: [UserField]) -> [UserField] {
+        UserField.allCases.filter { !unwanted.contains($0) }
+    }
+}
