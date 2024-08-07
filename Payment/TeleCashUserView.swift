ec5d894e142ad864a07ad4853a2e133b2ce8c4ae
Uwe Tilemann <uwe.tilemann@snabble.io>
2024-08-07 08:52:33 +0200

    Add UIKit TeleCashUserViewController

--- Payment/TeleCashUserView.swift
+++ Payment/TeleCashUserView.swift
@@ -9,19 +9,55 @@ import SwiftUI
 
 import SnabbleUser
 
-open class TeleCashUserViewController: UIHostingController<TeleCashUserView> {
-}
-
-struct TeleCashUserView: View {
-    let userFields = UserField.fieldsWithout([.state, .dateOfBirth])
+class UserObservable: ObservableObject {
+    @Published var user: User
     
-    var body: some View {
-        UserView(fields: userFields) { user in
-            print("got User:", user)
-        }
+    init(user: User) {
+        self.user = user
     }
 }
 
+protocol UserViewProxy {
+    func userInfoAvailable(user: User)
+}
+
+open class TeleCashUserViewController: UIHostingController<TeleCashUserView> {
+    var userObserver: UserObservable
+    
+    public weak var delegate: UserViewProxy?
+
+    init(user: User) {
+        self.userObserver = UserObservable(user: user)
+        let teleCashUserView = TeleCashUserView(user: $userObserver.user)
+        super.init(rootView: teleCashUserView)
+    }
+
+    @objc required dynamic init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        userObserver.$user.sink { updatedUser in
+            self.delegate?.userInfoAvailable(user: updatedUser)
+        }.store(in: &cancellables)
+    }
+
+    private var cancellables = Set<AnyCancellable>()
+}
+
+struct TeleCashUserView: View {
+    @Binding var user: User
+    let userFields = UserField.fieldsWithout([.state, .dateOfBirth])
+    
+    var body: some View {
+        UserView(fields: userFields) { updatedUser in
+            print("got User:", updatedUser)
+            self.user = updatedUser
+        }
+    }
+}
 
 #Preview {
     TeleCashUserView()
