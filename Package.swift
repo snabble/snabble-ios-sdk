c9477bd585f1674434b09f20005747c3d0900d65
Andreas Osberghaus <148758+mrgrauel@users.noreply.github.com>
2024-09-10 11:30:44 +0200

    [GENERAL] Release 0.60.0 (#381)

--- Package.swift
+++ Package.swift
@@ -58,7 +58,7 @@ let package = Package(
         .package(url: "https://github.com/lachlanbell/SwiftOTP", from: "3.0.2"),
         .package(url: "https://github.com/datatheorem/TrustKit.git", from: "3.0.4"),
         .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: "4.2.2"),
-        .package(url: "https://github.com/groue/GRDB.swift.git", from: "6.29.2"),
+        .package(url: "https://github.com/groue/GRDB.swift.git", from: "6.29.3"),
         .package(url: "https://github.com/weichsel/ZIPFoundation.git", .upToNextMajor(from: "0.9.19")),
         .package(url: "https://github.com/datatrans/ios-sdk.git", from: "3.7.0"),
         .package(url: "https://github.com/sberrevoets/SDCAlertView.git", from: "12.0.4"),
