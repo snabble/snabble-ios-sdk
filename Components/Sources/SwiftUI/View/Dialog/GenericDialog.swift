50d9634bffe5953bec992bada9a292ac8ef5f33e
Andreas Osberghaus <andreas.osberghaus@snabble.io>
2024-10-17 12:39:05 +0200

    Uses genericDialog for Toast

--- Components/Sources/SwiftUI/View/Dialog/GenericDialog.swift
+++ Components/Sources/SwiftUI/View/Dialog/GenericDialog.swift
@@ -12,15 +12,17 @@ import Combine
 public struct GenericDialog<DialogContent: View>: ViewModifier {
     @Binding var isPresented: Bool
     let onDismiss: (() -> Void)?
-    let duration: TimeInterval
+    let duration: TimeInterval?
     let dialogContent: DialogContent
     
     @State private var isFullscreenCoverPresented: Bool = false
     @State private var isFullScreenCoverVisible: Bool = false
     
+    @State private var workItem: DispatchWorkItem?
+    
     public init(isPresented: Binding<Bool>,
                 onDismiss: (() -> Void)?,
-                duration: TimeInterval,
+                duration: TimeInterval?,
                 @ViewBuilder content: () -> DialogContent) {
         _isPresented = isPresented
         self.onDismiss = onDismiss
@@ -49,11 +51,14 @@ public struct GenericDialog<DialogContent: View>: ViewModifier {
                 }
                 .onAppear {
                     isFullScreenCoverVisible = true
+                    enableAutomaticDismiss(duration: duration)
+                    UIImpactFeedbackGenerator(style: .light)
+                        .impactOccurred()
                 }
             }
             .transaction({ transaction in
                 transaction.disablesAnimations = true
-                transaction.animation = .linear(duration: duration)
+                transaction.animation = .linear(duration: 0.3)
             })
             .onChange(of: isPresented) { _, newValue in
                 if newValue {
@@ -68,12 +73,34 @@ public struct GenericDialog<DialogContent: View>: ViewModifier {
                 }
             }
     }
+    
+    private func enableAutomaticDismiss(duration: TimeInterval?) {
+        if let duration, duration > 0 {
+            workItem?.cancel()
+            
+            let task = DispatchWorkItem {
+                dismiss()
+            }
+            
+            workItem = task
+            DispatchQueue.main.asyncAfter(deadline: .now() + duration, execute: task)
+        }
+    }
+    
+    private func dismiss() {
+        withAnimation {
+            isFullScreenCoverVisible = false
+        }
+        
+        workItem?.cancel()
+        workItem = nil
+    }
 }
 
 public extension View {
-    func dialog<DialogContent: View>(isPresented: Binding<Bool>,
+    func genericDialog<DialogContent: View>(isPresented: Binding<Bool>,
                                      onDismiss: (() -> Void)? = nil,
-                                     duration: TimeInterval = 0.3,
+                                     duration: TimeInterval? = nil,
                                      @ViewBuilder content: @escaping () -> DialogContent) -> some View {
         modifier(GenericDialog(
             isPresented: isPresented,
