912d185a9c77bbd77627f36db8b29395994c934f
Uwe Tilemann <uwe.tilemann@snabble.io>
2024-10-25 10:48:17 +0200

    Remove Coordinator

--- Components/Sources/SwiftUI/View/ContainerView.swift
+++ Components/Sources/SwiftUI/View/ContainerView.swift
@@ -9,56 +9,28 @@ import SwiftUI
 
 public struct ContainerView: UIViewControllerRepresentable {
     public let viewController: UIViewController
-    
-    @Binding public var isPresented: Bool
-    
+        
     public init(viewController: UIViewController) {
         self.viewController = viewController
-        self._isPresented = .constant(true)
     }
-    
-    public init(viewController: UIViewController, isPresented: Binding<Bool>) {
-        self.viewController = viewController
-        self._isPresented = isPresented
-    }
-    
+        
     public func makeUIViewController(context: Context) -> UIViewController {
-        return ContainerViewController(coordinator: context.coordinator)
+        return ContainerViewController(viewController: viewController)
     }
     
-    public func makeCoordinator() -> Coordinator {
-        Coordinator(parent: self)
-    }
-
     // swiftlint:disable:next no_empty_block
     public func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
     
-    public class Coordinator: NSObject {
-        let parent: ContainerView
-        
-        init(parent: ContainerView) {
-            self.parent = parent
-        }
-                
-        var viewController: UIViewController {
-            parent.viewController
-        }
-    }
-    
     class ContainerViewController: UIViewController {
-        private let coordinator: Coordinator
-        
-        init(coordinator: Coordinator) {
-            self.coordinator = coordinator
+        public let viewController: UIViewController
+
+        init(viewController: UIViewController) {
+            self.viewController = viewController
             super.init(nibName: nil, bundle: nil)
         }
         
         required init?(coder: NSCoder) {
             fatalError("init(coder:) has not been implemented")
-        }
-        
-        var viewController: UIViewController {
-            coordinator.viewController
         }
         
         override func viewDidLoad() {
