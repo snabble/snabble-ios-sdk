a9fee70f52c1ca5e494014f7e134c5bec0ee93b9
Uwe Tilemann <uwe.tilemann@snabble.io>
2025-08-06 11:38:57 +0200

    Cleanup code

--- UI/Sources/Teaser/TeaserDetailView.swift
+++ UI/Sources/Teaser/TeaserDetailView.swift
@@ -7,49 +7,10 @@
 
 import UIKit
 import SwiftUI
-import WebKit
 
 import SnabbleCore
 import SnabbleComponents
 import SnabbleAssetProviding
-
-extension String {
-    var extractYouTubeID: String? {
-        guard let url = URL(string: self),
-              let components = URLComponents(url: url, resolvingAgainstBaseURL: false),
-              let queryItems = components.queryItems else {
-            return nil
-        }
-
-        return queryItems.first(where: { $0.name == "v" })?.value
-    }
-}
-struct YouTubeView: UIViewRepresentable {
-    let videoID: String
-
-    func makeUIView(context: Context) -> WKWebView {
-        let webView = WKWebView()
-        webView.scrollView.isScrollEnabled = false
-        webView.configuration.allowsInlineMediaPlayback = true
-        return webView
-    }
-
-    func updateUIView(_ uiView: WKWebView, context: Context) {
-        let embedHTML = """
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <meta name="viewport" content="initial-scale=1.0, maximum-scale=1.0">
-            <style>body { margin: 0; padding: 0; }</style>
-        </head>
-        <body>
-            <iframe width="100%" height="280px" src="https://www.youtube.com/embed/\(videoID)?playsinline=1" frameborder="0" allowfullscreen></iframe>
-        </body>
-        </html>
-        """
-        uiView.loadHTMLString(embedHTML, baseURL: nil)
-    }
-}
 
 public struct TeaserDetailView: View {
     @Environment(\.openURL) var openURL
@@ -138,9 +99,7 @@ public struct TeaserDetailView: View {
                 image = initialImage
             }
 
-            if let videoUrl = teaser.videoUrl {
-                
-            } else if let urlString = teaser.detailImageUrl {
+            if let urlString = teaser.detailImageUrl {
                 await loadImage(urlString)
             } else {
                 if image == nil, let urlString = teaser.imageUrl {
