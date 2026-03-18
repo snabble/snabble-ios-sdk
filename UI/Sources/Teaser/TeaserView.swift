23ae8efb5792f1746436f62b7d528d6183d06596
Uwe Tilemann <uwe.tilemann@snabble.io>
2026-03-18 16:27:41 +0100

    Update TeaserView.swift

--- UI/Sources/Teaser/TeaserView.swift
+++ UI/Sources/Teaser/TeaserView.swift
@@ -21,7 +21,7 @@ extension CustomizationConfig.Teaser: Hashable {
 }
 
 public struct TeaserView: View {
-    @State public var model: TeaserModel
+    let model: TeaserModel
     @State public var activePage: CustomizationConfig.Teaser?
 
     public let onNavigationPublisher: PassthroughSubject<(teaser: CustomizationConfig.Teaser, image: UIImage?), Never>
@@ -30,86 +30,69 @@ public struct TeaserView: View {
         model: TeaserModel,
         activePage: CustomizationConfig.Teaser? = nil,
         actionPublisher: PassthroughSubject<(teaser: CustomizationConfig.Teaser, image: UIImage?), Never> = .init()) {
-        self.model = model
+        self.model = model // State(wrappedValue: model)
         self.activePage = activePage
         self.onNavigationPublisher = actionPublisher
     }
 
     public var body: some View {
-        VStack {
-            VStack(spacing: 18) {
-                Text(Asset.localizedString(forKey: "Snabble.Teaser.title"))
-                    .font(.font("SnabbleUI.CustomFont.header", size: 20, relativeTo: .body, domain: nil))
-                    .fontWeight(.bold)
-                    .foregroundStyle(Color.projectPrimary())
-                    .padding(.leading, 7)
-                
-                ScrollView(.horizontal) {
-                    HStack(spacing: 0) {
-                        ForEach(model.teasers, id: \.self) { teaser in
-                            TeaserItemView(teaser: teaser) { image in
-                                onNavigationPublisher.send((teaser, image))
-                            }
-                            .environment(model)
-                            .clipShape(RoundedRectangle(cornerRadius: 12).inset(by: 1))
-                            .background(
-                                RoundedRectangle(cornerRadius: 12)
-                                    .fill(Color.systemBackground)
-                            )
-                        }
-                        .padding(.horizontal, 25)
-                        .containerRelativeFrame(.horizontal, count: 1, spacing: 0)
-                    }
-                    .scrollTargetLayout()
-                }
-                .scrollBounceBehavior(.basedOnSize, axes: [.horizontal])
-                .scrollTargetBehavior(.viewAligned)
-                .scrollPosition(id: $activePage)
-                .scrollIndicators(.never)
-
-                pagingControl
-                    .opacity(model.teasers.count > 1 ? 1 : 0)
-            }
-            .padding(.top, 32)
-            .padding(.bottom, 24)
-        }
-        .background(Color.projectFaq())
-        .font(.font("SnabbleUI.CustomFont.teaser", size: 17, relativeTo: .body, domain: nil))
-    }
-    
-    @ViewBuilder
-    var pagingControl: some View {
-        HStack {
-            ForEach(model.teasers) { page in
-                Button {
-                    withAnimation {
-                        activePage = page
-                    }
-                } label: {
-                    Image(systemName: "circle.fill")
-                        .font(.system(size: 8))
-                        .foregroundStyle(activePage == page ? Color.projectPrimary() : .white)
-                }
-            }
-        }
-        .task {
-            if activePage == nil {
-                activePage = model.teasers.first
-            }
-        }
-    }
-}
-
-public struct TeaserComponent: View {
-    @State private var model = TeaserModel()
-
-    public init(model: TeaserModel = .init()) {
-        self.model = model
-    }
-
-    public var body: some View {
         if !model.teasers.isEmpty {
-            TeaserView(model: model)
+            VStack {
+                VStack(spacing: 18) {
+                    Text(Asset.localizedString(forKey: "Snabble.Teaser.title"))
+                        .font(.font("SnabbleUI.CustomFont.header", size: 20, relativeTo: .body, domain: nil))
+                        .fontWeight(.bold)
+                        .foregroundStyle(Color.projectPrimary())
+                        .padding(.leading, 7)
+                    
+                    ScrollView(.horizontal) {
+                        HStack(spacing: 0) {
+                            ForEach(model.teasers, id: \.self) { teaser in
+                                TeaserItemView(teaser: teaser) { image in
+                                    onNavigationPublisher.send((teaser, image))
+                                }
+                                .environment(model)
+                                .clipShape(RoundedRectangle(cornerRadius: 12).inset(by: 1))
+                                .background(
+                                    RoundedRectangle(cornerRadius: 12)
+                                        .fill(Color.systemBackground)
+                                )
+                            }
+                            .padding(.horizontal, 25)
+                            .containerRelativeFrame(.horizontal, count: 1, spacing: 0)
+                        }
+                        .scrollTargetLayout()
+                    }
+                    .scrollBounceBehavior(.basedOnSize, axes: [.horizontal])
+                    .scrollTargetBehavior(.viewAligned)
+                    .scrollPosition(id: $activePage)
+                    .scrollIndicators(.never)
+                    
+                    HStack {
+                        ForEach(model.teasers) { page in
+                            Button {
+                                withAnimation {
+                                    activePage = page
+                                }
+                            } label: {
+                                Image(systemName: "circle.fill")
+                                    .font(.system(size: 8))
+                                    .foregroundStyle(activePage == page ? Color.projectPrimary() : .white)
+                            }
+                        }
+                    }
+                    .opacity(model.teasers.count > 1 ? 1 : 0)
+                    .task {
+                        if activePage == nil {
+                            activePage = model.teasers.first
+                        }
+                    }
+                }
+                .padding(.top, 32)
+                .padding(.bottom, 24)
+            }
+            .background(Color.projectFaq())
+            .font(.font("SnabbleUI.CustomFont.teaser", size: 17, relativeTo: .body, domain: nil))
         }
     }
 }
