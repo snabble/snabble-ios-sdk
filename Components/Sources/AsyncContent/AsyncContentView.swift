af00fe1b8fbb9830e49b3c10a17a993bc77fd675
Uwe Tilemann <uwe.tilemann@snabble.io>
2026-03-23 10:37:08 +0100

    Create AsyncContentView.swift

---
+++ Components/Sources/AsyncContent/AsyncContentView.swift
@@ -0,0 +1,89 @@
+//
+//  AsyncContentView.swift
+//  AutoLayout-Helper
+//
+//  Created by Andreas Osberghaus on 08.09.22.
+//
+
+import Foundation
+import SwiftUI
+
+import SnabbleAssetProviding
+
+public protocol LoadableObject {
+    associatedtype Output
+    var state: LoadingState<Output> { get }
+    func load()
+}
+
+public enum LoadingState<Value> {
+    case idle
+    case loading
+    case failed(Error)
+    case loaded(Value)
+    case empty
+}
+
+public struct AsyncContentView<Source: LoadableObject, Content: View, Empty: View, ErrorView: View>: View {
+    let source: Source
+    var content: (Source.Output) -> Content
+    var empty: (() -> Empty)?
+    var errorView: ((_ error: Error) -> ErrorView)?
+
+    public init(source: Source, content: @escaping (Source.Output) -> Content) where Empty == EmptyView, ErrorView == EmptyView {
+        self.source = source
+        self.content = content
+        self.empty = nil
+        self.errorView = nil
+    }
+
+    public init(source: Source, content: @escaping (Source.Output) -> Content, empty: @escaping () -> Empty) where ErrorView == EmptyView {
+        self.source = source
+        self.content = content
+        self.empty = empty
+        self.errorView = nil
+    }
+    public init(source: Source, content: @escaping (Source.Output) -> Content, empty: @escaping () -> Empty, errorView: @escaping (Error) -> ErrorView) {
+        self.source = source
+        self.content = content
+        self.empty = empty
+        self.errorView = errorView
+    }
+
+    var isLoading: Bool {
+        if case .loading = source.state {
+            return true
+        }
+        return false
+    }
+
+    public var body: some View {
+        switch source.state {
+        case .idle:
+            Color.clear.onAppear(perform: source.load)
+        case .loading:
+            ProgressView()
+        case .failed(let error):
+            if let errorView {
+                errorView(error)
+            } else {
+                VStack(spacing: 16) {
+                    Text(Asset.localizedString(forKey: "Snabble.Error.defaultMessage"))
+                        .multilineTextAlignment(.center)
+                    SecondaryButtonView(title: Asset.localizedString(forKey: "Snabble.Error.retry")) {
+                        self.source.load()
+                    }
+                }
+                .padding()
+            }
+        case .empty:
+            if let empty {
+                empty()
+            } else {
+                EmptyView()
+            }
+        case .loaded(let output):
+            content(output)
+        }
+    }
+}
