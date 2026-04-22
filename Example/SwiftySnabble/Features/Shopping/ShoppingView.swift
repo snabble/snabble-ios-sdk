13bf774f043d0a2c95af3ac6e74cbd9919f86cdb
Uwe Tilemann <uwe.tilemann@snabble.io>
2026-04-22 10:38:44 +0200

    Show NavigationBar

--- Example/SwiftySnabble/Features/Shopping/ShoppingView.swift
+++ Example/SwiftySnabble/Features/Shopping/ShoppingView.swift
@@ -72,6 +72,7 @@ struct ShoppingView: View {
         NavigationStack {
             ShopperView(model: shopper)
                 .actionState()
+                .navigationBarHidden(false)
         }
     }
 }
