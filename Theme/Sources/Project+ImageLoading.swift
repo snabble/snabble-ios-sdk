3307577128c29ad29c3e4bdca6c167067ff558ae
Uwe Tilemann <dehale@me.com>
2026-06-02 11:43:58 +0200

    Make async

--- Theme/Sources/Project+ImageLoading.swift
+++ Theme/Sources/Project+ImageLoading.swift
@@ -10,26 +10,27 @@ import UIKit
 import SnabbleCore
 
 extension Project {
-    public func fetchImage(urlString: String, completion: @escaping @Sendable (UIImage?) -> Void) {
-        request(.get, urlString, timeout: 3) { request in
-            guard let request = request else {
-                return completion(nil)
-            }
-            let session = Snabble.urlSession
-            let task = session.dataTask(with: request) { data, response, error in
-                if let error = error {
-                    Log.error("Error landing page image download: \(error)")
-                    return completion(nil)
+    public func fetchImage(urlString: String) async -> UIImage? {
+        await withCheckedContinuation { continuation in
+            request(.get, urlString, timeout: 3) { request in
+                guard let request = request else {
+                    return continuation.resume(returning: nil)
                 }
-                
-                let statusCode = (response as? HTTPURLResponse)?.statusCode ?? 0
-                guard statusCode == 200, let data = data else {
-                    Log.error("Error landing page image download for \(name): \(statusCode)")
-                    return completion(nil)
+                let session = Snabble.urlSession
+                let task = session.dataTask(with: request) { data, response, error in
+                    if let error = error {
+                        Log.error("Error landing page image download: \(error)")
+                        return continuation.resume(returning: nil)
+                    }
+                    let statusCode = (response as? HTTPURLResponse)?.statusCode ?? 0
+                    guard statusCode == 200, let data = data else {
+                        Log.error("Error landing page image download for \(name): \(statusCode)")
+                        return continuation.resume(returning: nil)
+                    }
+                    continuation.resume(returning: UIImage(data: data))
                 }
-                completion(UIImage(data: data))
+                task.resume()
             }
-            task.resume()
         }
     }
 }
